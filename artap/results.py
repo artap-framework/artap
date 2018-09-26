64cda51f2999dbaa19762ac838b1f9d43b51f943
David Pánek <panek50@kte.zcu.cz>
2018-09-26 10:37:40 +0200

    minor corrections

--- artap/results.py
+++ artap/results.py
@@ -1,9 +1,10 @@
+import matplotlib
+matplotlib.use('Agg')
+import pylab as pl
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 from matplotlib import rc
-import pylab as pl
-import matplotlib
-matplotlib.use('Agg')
+
 
 
 class Results:
