c3a5eb5b3bc4ca895aee3777260e7d6fc55efd54
David Pánek <panek50@kte.zcu.cz>
2018-10-09 12:47:35 +0200

    some improvements
    Former-commit-id: 233a3fc23446635b47d34fc3e5e3770cfa2c5c6a

--- artap_server/templates.py
+++ artap_server/templates.py
@@ -1,5 +1,4 @@
 import string
-import sqlite3
 import os
 
 from artap.enviroment import Enviroment
@@ -9,6 +8,8 @@ from artap.problem import ProblemDataStore
 
 class WebPagesWriter:
 
+    data_dirs = ["../artap/tests/workspace/", "../artap/projects"]
+
     def __init__(self):
         self.table = self.read_problems()
         self.static_dir = Enviroment.artap_root + "../artap_server/static/"
@@ -16,24 +17,46 @@ class WebPagesWriter:
 
     def read_problems(self):
         table = [["Problem", "Description"]]
-        problems = os.listdir("../artap/projects/")
-        for problem in problems:
-            table.append([problem, ""])
-
+        for path in self.data_dirs:
+            problems = os.listdir(path)
+            i = 0
+            for problem in problems:
+                table.append([i, path, problem])
+                i += 1
         return table
 
     def problems(self):
-        html_page = self.table_to_html(self.table, link_column=0)
+        html_page = self.table_to_html(self.table, link_column=0, page="calculation")
         return html_page
+
+    def calculation_details(self, id):
+        table = [["Problem", "Description"]]
+        index = int(id) + 1
+        path = self.table[index][1] + self.table[index][2] + "/"
+        items = os.listdir(path)
+        calculations = []
+
+        for item in items:
+            if os.path.isdir(path + item):
+                calculations.append(path + item + "/")
+        i = 0
+        self.database_files = []
+        for problem in calculations:
+            file = problem + "/data.sqlite"
+            self.database_files.append(file)
+            data_store = SqliteDataStore(database_file=file)
+            problem = ProblemDataStore(data_store)
+            table.append([i, problem.name, problem.description])
+            i += 1
+        return self.table_to_html(table, link_column=1, page="problem")
 
     def problem_details(self, id):
-        data_store = SqliteDataStore(new_database=False, problem_id=id)
+        data_store = SqliteDataStore(database_file=self.database_files[int(id)])
         problem = ProblemDataStore(data_store)
-        html_page = self.table_to_html(problem.to_table())
-
+        html_page = self.table_to_html(problem.to_table(), link_column=1, page="item")
         return html_page
 
-    def table_to_html(self, table, link_column=None):
+    def table_to_html(self, table, link_column=None, page=None):
         table_template = string.Template(""" <table class="blueTable">
                                     $table
                                     </table> """)
@@ -56,7 +79,7 @@ class WebPagesWriter:
             line = table[i]
             for j in range(len(line)):
                 if (link_column != None) and (j == link_column):
-                    row_content += cell_hyperlink.substitute(cell=line[j], link="problem?id=" + str(line[0]))
+                    row_content += cell_hyperlink.substitute(cell=line[j], link= page +"?id=" + str(line[0]))
                 else:
                     row_content += cell.substitute(cell=line[j], link="index?id=" + str(line[0]))
             row_content += "\n"
