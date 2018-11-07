5ec04105c54bdff4fa92c031d12328d09e24df13
David Pánek <panek50@kte.zcu.cz>
2018-11-07 15:35:28 +0100

    minor corrections
    Former-commit-id: 5ed8d201fd9ec37273b704b4374f358e2a410941 [formerly da19eedf45470aa95268726add3044d3499d9595 [formerly b8423cda06e5bd0deb6a96ab2b0cea87aa94a8db]]
    Former-commit-id: 267906eccfa5279dd7eb79ffe160c7f0e1c3c1da
    Former-commit-id: 32bcc43ed39c1d9898237e47700c28df7e8b0ae0

--- artap/tests/test_graphical_results.py
+++ artap/tests/test_graphical_results.py
@@ -9,14 +9,10 @@ class TestDataStore(unittest.TestCase):
 
     def test_local_problem_data_store(self):
         database_file = "." + os.sep + "workspace" + os.sep + "common_data" + os.sep + "data.sqlite"
-        # working_dir = "." + os.sep + "workspace" + os.sep + "common_data" + os.sep
-        # data_store = SqliteDataStore(database_file=database_file, working_dir=working_dir)
         data_store = SqliteDataStore(database_file=database_file)
         problem = ProblemDataStore(data_store)
         results = GraphicalResults(problem)
         results.plot_all_individuals()
-        results.plot_populations()
-
 
 if __name__ == '__main__':
     unittest.main()
