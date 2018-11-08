70b0460e791985e3a64c453e44071a2099d4dbe1
Pavel Karban <pkarban@gmail.com>
2018-11-08 09:39:06 +0100

    3rdparty introduced
    Former-commit-id: 8b3eebbf6e4723b7fc1fb7b25ec321451beddb33 [formerly e5af9f948426193002fcece0fa2b1ab11206572a [formerly 3b9bc08b2c98b7ef8b058910608f9cae982af012]]
    Former-commit-id: bc966e2294246811d03e9e5146dac5453c54fb93
    Former-commit-id: f837bc9137a423c6999fcf08c600ffb6a406ba96

---
+++ 3rdparty/submodules.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+DIR=$PWD
+
+# git submodule add git@edison.fel.zcu.cz:agros/RDOLab/3rdparty/nlopt2.git 3rdparty/nlopt2
+# git submodule add git@edison.fel.zcu.cz:agros/RDOLab/3rdparty/platypus.git 3rdparty/platypus
+# git submodule add git@edison.fel.zcu.cz:agros/RDOLab/3rdparty/bayesopt.git 3rdparty/bayesopt
+
+# update
+git submodule update 
+
+# nlopt2
+cd $DIR/nlopt2
+mkdir -p build
+cd build
+cmake ..
+make -j4
+cp $DIR/nlopt2/build/src/swig/_nlopt.so $DIR/../artap/lib/
+strip $DIR/../artap/lib/_nlopt.so
+
+# BayesOpt
+
+cd $DIR/bayesopt
+mkdir -p build
+cd build
+cmake ..
+make -j4
+cp $DIR/bayesopt/build/lib/bayesopt.so $DIR/../artap/lib/
+strip $DIR/../artap/lib/bayesopt.so
+
+
+
