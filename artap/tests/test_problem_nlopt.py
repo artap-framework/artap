2fd5782ed4bbb8c4c4d6235c9cac5a81861a83a1
Pavel Karban <pkarban@gmail.com>
2018-11-09 09:34:51 +0100

    nlopt - test improvement
    Former-commit-id: febbfd3750efd66e291f8faa02b19b2d57c818ca [formerly 1e54457d92808269e11d139a88489a892d009167 [formerly 6f46265177651a8f4dac85bcfaa943103f28d842]]
    Former-commit-id: 45bbb2cd7a0c69ba67ed7e9473130b5f5a9dec1f
    Former-commit-id: 3dd970fd94e2901f7998935d15cd39d707916829

--- artap/tests/test_problem_nlopt.py
+++ artap/tests/test_problem_nlopt.py
@@ -2,50 +2,92 @@ import unittest
 
 from artap.problem import Problem
 from artap.algorithm_nlopt import NLopt
+from artap.algorithm_nlopt import GN_DIRECT_L
+from artap.algorithm_nlopt import GN_DIRECT_L_RAND
+from artap.algorithm_nlopt import GN_MLSL
+from artap.algorithm_nlopt import GN_CRS2_LM
+from artap.algorithm_nlopt import GN_ISRES
+from artap.algorithm_nlopt import GN_ESCH
 from artap.algorithm_nlopt import LN_BOBYQA
+from artap.algorithm_nlopt import LN_COBYLA
 from artap.algorithm_nlopt import LN_NELDERMEAD
+from artap.algorithm_nlopt import LN_SBPLX
+from artap.algorithm_nlopt import LN_PRAXIS
+from artap.algorithm_nlopt import LN_AUGLAG_EQ
 
-from artap.benchmark_functions import Rosenbrock
+from artap.results import Results
+
+from artap.benchmark_functions import Booth
 
 class MyProblem(Problem):
     """ Describe simple one objective optimization problem. """
     def __init__(self, name):
-        parameters = {'x_1': {'initial_value': 2.5, 'bounds': [-30, 30], 'precision': 1e-1},
-                      'x_2': {'initial_value': 1.5, 'bounds': [-30, 30], 'precision': 1e-1}}
+        parameters = {'x_1': {'initial_value': 2.5, 'bounds': [-10, 10], 'precision': 1e-1},
+                      'x_2': {'initial_value': 1.5, 'bounds': [-10, 10], 'precision': 1e-1}}
+
+        # costs = {'F': {'type': Problem.MINIMIZE, 'value': 0.0}}
         costs = ['F']
 
         working_dir = "./workspace/common_data/"
         super().__init__(name, parameters, costs, working_dir=working_dir, save_data=False)
 
     def eval(self, x):
-        return Rosenbrock.eval(x)
+        return Booth.eval(x)
 
 
 class TestNLoptOptimization(unittest.TestCase):
     """ Tests simple one objective optimization problem."""
-    
-    def test_local_problem_nlopt_bobyqa(self):
-        problem = MyProblem("LocalPythonProblemNLopt_LN_BOBYQA")
+
+    def run_test(self, method, n_iterations=100):
+        problem = MyProblem("LocalPythonProblemNLopt")
         algorithm = NLopt(problem)
         algorithm.options['verbose_level'] = 0
-        algorithm.options['algorithm'] = LN_BOBYQA
-        algorithm.options['n_iterations'] = 200
+        algorithm.options['algorithm'] = method
+        algorithm.options['xtol_rel'] = 1e-4
+        algorithm.options['n_iterations'] = n_iterations
         algorithm.run()
 
-        optimum = problem.populations[-1].individuals[-1].costs[0]  # Takes last cost function
-        self.assertAlmostEqual(optimum, 0)
+        # optimum = problem.populations[-1].individuals[-1].costs[0]  # Takes last cost function
 
+        results = Results(problem)
+        optimum = results.find_minimum(name='F').costs[0]
+        self.assertAlmostEqual(optimum, 0, places=1)
 
-    def test_local_problem_nlopt_ln_neldermead(self):
-        problem = MyProblem("LocalPythonProblemNLopt_LN_NELDERMEAD")
-        algorithm = NLopt(problem)
-        algorithm.options['verbose_level'] = 0
-        algorithm.options['algorithm'] = LN_NELDERMEAD
-        algorithm.options['n_iterations'] = 150
-        algorithm.run()
+    def test_local_problem_nlopt_GN_DIRECT_L(self):
+        self.run_test(GN_DIRECT_L, 400)
 
-        optimum = problem.populations[-1].individuals[-1].costs[0]  # Takes last cost function
-        self.assertAlmostEqual(optimum, 0)
+    #def test_local_problem_nlopt_GN_DIRECT_L_RAND(self):
+    #    self.run_test(GN_DIRECT_L_RAND, 100)
+
+    def test_local_problem_nlopt_GN_MLSL(self):
+        self.run_test(GN_MLSL, 500)
+
+    def test_local_problem_nlopt_GN_CRS2_LM(self):
+        self.run_test(GN_CRS2_LM, 1000)
+
+    def test_local_problem_nlopt_GN_ISRES(self):
+        self.run_test(GN_ISRES, 3000)
+
+    def test_local_problem_nlopt_GN_ESCH(self):
+        self.run_test(GN_ESCH, 5000)
+
+    def test_local_problem_nlopt_LN_BOBYQA(self):
+        self.run_test(LN_BOBYQA)
+
+    def test_local_problem_nlopt_LN_COBYLA(self):
+        self.run_test(LN_COBYLA)
+
+    def test_local_problem_nlopt_LN_NELDERMEAD(self):
+        self.run_test(LN_NELDERMEAD)
+
+    def test_local_problem_nlopt_LN_SBPLX(self):
+        self.run_test(LN_SBPLX, 200)
+
+    def test_local_problem_nlopt_LN_AUGLAG_EQ(self):
+        self.run_test(LN_AUGLAG_EQ)
+
+    def test_local_problem_nlopt_LN_PRAXIS(self):
+        self.run_test(LN_PRAXIS)
 
 if __name__ == '__main__':
     unittest.main()
