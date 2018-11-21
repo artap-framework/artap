7a0235d382bf9c366d2dfbe59d7e0a9ca85dd429
Tamás <orosz.tamas@yahoo.com>
2018-11-21 14:01:38 +0100

    bug: if max:processes == 1 in population (nsga2/genetic)
    Former-commit-id: 78d066944ae30b8445c365bf868e0ae389446648 [formerly ca155969913375e1fefc51e97fca150fb876b909 [formerly 379bc25ab87cfe116b5a135e08fd167f7089e97b]]
    Former-commit-id: 538e0c64f132d666adaa9d6f16efd8ca552a01fc
    Former-commit-id: 19786158f6db4285e0862a380859e3777b0642e4

--- artap/tests/test_problem_nsga2.py
+++ artap/tests/test_problem_nsga2.py
@@ -9,11 +9,13 @@ from artap.results import GraphicalResults, Results
 class MyProblem(Problem):
     """ Describe simple one objective optimization problem. """
     def __init__(self, name):
-        parameters = {'x_1': {'initial_value': 2.5, 'bounds': [-32, 32], 'precision': 1e-1},
-                      'x_2': {'initial_value': 1.5, 'bounds': [-32, 32], 'precision': 1e-1}}
+        parameters = {'x_1': {'initial_value': 2.5, 'bounds': [0, 5], 'precision': 1e-1},
+                      'x_2': {'initial_value': 1.5, 'bounds': [0, 3], 'precision': 1e-1}}
         costs = ['F_1', 'F_2']
+
         working_dir = "." + os.sep + "workspace" + os.sep + "common_data" + os.sep
-        super().__init__(name, parameters, costs, working_dir=working_dir, save_data=True)
+
+        super().__init__(name, parameters, costs, working_dir=working_dir, save_data=True, max_processes=2)
 
     def eval(self, x):
         return Binh_and_Korn.eval(x)
@@ -21,29 +23,29 @@ class MyProblem(Problem):
     def eval_constraints(self, x):
         return Binh_and_Korn.constraints(x)
 
+class TestNSGA2Optimization(unittest.TestCase):
+    """ Tests simple one objective optimization problem."""
 
-# class TestNSGA2Optimization(unittest.TestCase):
-#     """ Tests simple one objective optimization problem."""
-#
-#     def test_local_problem_nsga2(self):
-#         problem = MyProblem("LocalPythonProblemNSGA_II")
-#         algorithm = NSGA_II(problem)
-#         algorithm.options['max_population_number'] = 40
-#         algorithm.options['max_population_size'] = 100
-#         algorithm.run()
-#
-#         b = Results(problem)
-#         solution = b.pareto_values()
-#         # print(*solution[0], sep="\n")
-#
-#         x, y = zip(*solution)
-#
-#         wrong = 0
-#         for sol in solution:
-#             if abs(Binh_and_Korn.approx(sol[0])-sol[1]) > 0.1 * Binh_and_Korn.approx(sol[0]) and sol[0] > 20 and sol[0]< 70:
-#                 wrong += 1
-#
-#         self.assertEqual(wrong, 0)
+    def test_local_problem_nsga2(self):
+
+        problem = MyProblem("LocalPythonProblemNSGA_II")
+        algorithm = NSGA_II(problem)
+        algorithm.options['max_population_number'] = 40
+        algorithm.options['max_population_size'] = 100
+        algorithm.run()
+
+        b = Results(problem)
+        solution = b.pareto_values()
+        # print(*solution[0], sep="\n")
+
+        x, y = zip(*solution)
+
+        wrong = 0
+        for sol in solution:
+            if abs(Binh_and_Korn.approx(sol[0])-sol[1]) > 0.1 * Binh_and_Korn.approx(sol[0]) and sol[0] > 20 and sol[0]< 70:
+                wrong += 1
+
+        self.assertEqual(wrong, 0)
 
 
 class AckleyN2Test(Problem):
@@ -54,9 +56,7 @@ class AckleyN2Test(Problem):
                       'x_2': {'initial_value': 2.5, 'bounds': [-32, 32], 'precision': 1e-1}}
         costs = ['F_1']
         working_dir = "./workspace/common_data/"
-        max_processes = 10
-        super().__init__(name, parameters, costs, working_dir=working_dir, save_data=False, max_processes=max_processes)
-
+        super().__init__(name, parameters, costs, working_dir=working_dir, save_data=False, max_processes=2)
 
     def eval(self, x):
         return AckleyN2.eval(x)
@@ -71,8 +71,6 @@ class TestAckleyN2(unittest.TestCase):
         algorithm.options['max_population_number'] = 15
         algorithm.options['max_population_size'] = 100
         algorithm.run()
-        results = GraphicalResults(problem)
-        results.plot_all_individuals()
 
         b = Results(problem)
         optimum = b.find_minimum('F_1')  # Takes last cost function
