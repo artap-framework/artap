b15e77a0cc4df3f0fab7e6749f6af0a8be542df2
David Pánek <panek50@kte.zcu.cz>
2018-11-21 06:29:50 +0100

    merge
    Former-commit-id: d1369b583788baf784f4a06f5e5313796c5e0d27 [formerly 0f288194e373a28a29114f440aef23ceb64bb315 [formerly ba71cf275c569dd64c89ba7175ca1313795be525]]
    Former-commit-id: a4b08aa6d2076a5b456ed787870d6e747a67bd22
    Former-commit-id: 3b6f004ef5ac870e26783f224c12c6fdc19bf99a

--- artap/tests/test_problem_nsga2.py
+++ artap/tests/test_problem_nsga2.py
@@ -9,28 +9,28 @@ from artap.results import GraphicalResults, Results
 class MyProblem(Problem):
     """ Describe simple one objective optimization problem. """
     def __init__(self, name):
-        parameters = {'x_1': {'initial_value': 2.5, 'bounds': [0, 5], 'precision': 1e-1},
-                      'x_2': {'initial_value': 1.5, 'bounds': [0, 3], 'precision': 1e-1}}
+        parameters = {'x_1': {'initial_value': 2.5, 'bounds': [-32, 32], 'precision': 1e-1},
+                      'x_2': {'initial_value': 1.5, 'bounds': [-32, 32], 'precision': 1e-1}}
         costs = ['F_1', 'F_2']
-
         working_dir = "." + os.sep + "workspace" + os.sep + "common_data" + os.sep
-
         super().__init__(name, parameters, costs, working_dir=working_dir, save_data=True)
 
     def eval(self, x):
-        return Binh_and_Korn.eval(x)
+        return [AckleyN2.eval(x), -AckleyN2.eval(x)]
 
 
-class TestNSGA2Optimization(unittest.TestCase):
-    """ Tests simple one objective optimization problem."""
-    
-    def test_local_problem_nsga2(self):
-
-        problem = MyProblem("LocalPythonProblemNSGA_II")
-        algorithm = NSGA_II(problem)
-        algorithm.options['max_population_number'] = 10
-        algorithm.options['max_population_size'] = 100
-        algorithm.run()
+# class TestNSGA2Optimization(unittest.TestCase):
+#     """ Tests simple one objective optimization problem."""
+#
+#     def test_local_problem_nsga2(self):
+#
+#         problem = MyProblem("LocalPythonProblemNSGA_II")
+#         algorithm = NSGA_II(problem)
+#         algorithm.options['max_population_number'] = 20
+#         algorithm.options['max_population_size'] = 100
+#         algorithm.run()
+#         results = GraphicalResults(problem)
+#         results.plot_populations()
 
 
 class AckleyN2Test(Problem):
@@ -56,6 +56,8 @@ class TestAckleyN2(unittest.TestCase):
         algorithm.options['max_population_number'] = 15
         algorithm.options['max_population_size'] = 100
         algorithm.run()
+        results = GraphicalResults(problem)
+        results.plot_all_individuals()
 
         b = Results(problem)
         optimum = b.find_minimum('F_1')  # Takes last cost function
