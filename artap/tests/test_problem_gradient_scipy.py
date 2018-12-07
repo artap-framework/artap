047a9cdafb27a1684dd9d43de0382c75a90c7b16
David Pánek <panek50@kte.zcu.cz>
2018-12-07 15:27:53 +0100

    Added numerical differentiaton
    Former-commit-id: 86dde0af12aa4830c8ef9288f00b87adf869c20c

---
+++ artap/tests/test_problem_gradient_scipy.py
@@ -0,0 +1,77 @@
+import os
+import unittest
+from artap.problem import Problem
+from artap.individual import  Individual
+from artap.algorithm_scipy import ScipyOpt
+from artap.benchmark_functions import Rosenbrock, Ackley4Modified, AckleyN2
+from artap.results import Results
+from numpy import sin
+
+class MyProblem(Problem):
+    """ Describe simple one objective optimization problem. """
+    def __init__(self, name):
+        parameters = {'x_1': {'initial_value': 10}}
+        costs = ['F_1']
+
+        super().__init__(name, parameters, costs)
+        self.options['max_processes'] = 1
+
+    def eval(self, x):
+        result = 0
+        for i in x:
+            result += i*i
+
+        return result
+
+
+class TestSimpleOptimization(unittest.TestCase):
+    """ Tests simple one objective optimization problem."""
+
+    def test_local_problem(self):
+        problem = MyProblem("LocalPythonProblem")
+        algorithm = ScipyOpt(problem)
+        algorithm.options['algorithm'] = 'Nelder-Mead'
+        algorithm.options['tol'] = 1e-4
+        algorithm.run()
+
+        # results = Results(problem)
+        # optimum = results.find_minimum('F_1')
+        # self.assertAlmostEqual(optimum, 0)
+
+
+class AckleyN2Test(Problem):
+    """Test with a simple 2 variable Ackley N2 formula"""
+
+    def __init__(self, name):
+        parameters = {'x': {'initial_value': 2.13}, 'y': {'initial_value': 2.13}}
+        costs = ['F_1']
+
+        super().__init__(name, parameters, costs)
+        self.options['max_processes'] = 1
+
+    def eval(self, x):
+        return sin(x[1]) + sin(x[0])
+
+
+class TestAckleyN2(unittest.TestCase):
+    """ Tests simple one objective optimization problem."""
+
+    def test_local_problem(self):
+        problem = AckleyN2Test("LocalPythonProblem")
+        x = [3, 3]
+        individual = Individual(x, problem)
+        problem.evaluate_gradient(individual)
+        problem.evaluate_gradient_richardson(individual)
+
+        # algorithm = ScipyOpt(problem)
+        # algorithm.options['algorithm'] = 'Nelder-Mead'
+        # algorithm.options['tol'] = 1e-4
+        # algorithm.run()
+
+        #results = Results(problem)
+        #optimum = results.find_minimum('F_1')
+        #self.assertAlmostEqual(optimum, -200, 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
