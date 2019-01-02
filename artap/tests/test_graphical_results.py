758a651a1366881479f1363e3e23ffc26222fe98
David Pánek <panek50@kte.zcu.cz>
2019-01-02 15:40:11 +0100

    disabled local comsol problem
    Former-commit-id: f1b694abf151ba385320247c92299000891c5915

--- artap/tests/test_graphical_results.py
+++ artap/tests/test_graphical_results.py
@@ -8,16 +8,14 @@ from artap.benchmark_functions import Rosenbrock
 class TestDataStore(unittest.TestCase):
 
     def test_local_problem_data_store(self):
-        function = Rosenbrock()
-        function.plot()
-        # working_dir = "." + os.sep + "workspace" + os.sep + "common_data" + os.sep
-        # database_file = working_dir + "data.sqlite"
-        # data_store = SqliteDataStore(database_file=database_file)
-        # problem = ProblemDataStore(data_store, working_dir=working_dir)
-        #
-        # results = GraphicalResults(problem)
-        # results.plot_all_individuals()
-        # results.plot_populations()
+        working_dir = "." + os.sep + "workspace" + os.sep + "common_data" + os.sep
+        database_file = working_dir + "data.sqlite"
+        data_store = SqliteDataStore(database_file=database_file)
+        problem = ProblemDataStore(data_store, working_dir=working_dir)
+
+        results = GraphicalResults(problem)
+        results.plot_all_individuals()
+        results.plot_populations()
 
 
 if __name__ == '__main__':
