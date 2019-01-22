d14911cf49ce1ec76accbea84d8e306e532a66fc
David Pánek <panek50@kte.zcu.cz>
2019-01-22 16:22:03 +0100

    refactoring
    Former-commit-id: 1b953610e746d3fadbab88fa878b69a3e8954591 [formerly 1aff184bedd9f2cfa510d29d0c661775780902d3 [formerly fa6d59a48489155cc126d12ce4b1413f305b0e7c]]
    Former-commit-id: 2bda55724b49e4509bbf7ae56f92d6dc7de2b045
    Former-commit-id: 174087b5e04709cc1d6e3a968332060e74fc879d

---
+++ artap/tests/test_algorithm.py
@@ -0,0 +1,49 @@
+import unittest
+from artap.problem import Problem
+from artap.datastore import DummyDataStore
+from artap.individual import Individual
+from artap.algorithm import EvalAll
+
+
+class TestProblem(Problem):
+    """ Describe simple one objective optimization problem. """
+    def __init__(self, name):
+        parameters = {'x_1': {'initial_value': 10}}
+        costs = ['F_1']
+
+        super().__init__(name, parameters, costs, data_store=DummyDataStore(self))
+        self.options['max_processes'] = 1
+
+    def evaluate(self, x):
+        result = 0
+        for i in x:
+            result += i*i
+
+        return [result]
+
+
+class TestJob(unittest.TestCase):
+    """ Tests simple one objective optimization problem."""
+
+    def test_evaluate_serial(self):
+        problem = TestProblem("Test_Job")
+        i1 = Individual([1, 2, 2])
+        i2 = Individual([3, 3, 2])
+        algorithm = EvalAll(problem, [i1, i2])
+        algorithm.evaluate(individuals=[i1, i2])
+        self.assertEqual(i1.costs, [9])
+        self.assertEqual(i2.costs, [22])
+
+    def test_evaluate_parallel(self):
+        problem = TestProblem("Test_Job")
+        problem.options['max_processes'] = 4
+        i1 = Individual([1, 2, 2])
+        i2 = Individual([3, 3, 2])
+        algorithm = EvalAll(problem, [i1, i2])
+        individuals = algorithm.evaluate(individuals=[i1, i2])
+        self.assertEqual(individuals[0].costs, [9])
+        self.assertEqual(individuals[1].costs, [22])
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ artap/tests/test_operators.py
@@ -0,0 +1,20 @@
+from artap.operators import SimulatedBinaryCrossover
+from artap.individual import Individual
+
+import unittest
+
+
+class TestSensitivity(unittest.TestCase):
+    """ Tests simple one objective optimization problem."""
+
+    def test_local_problem(self):
+        parameters = {'x_1': {'initial_value': 2.5, 'bounds': [0, 5], 'precision': 1e-1},
+                      'x_2': {'initial_value': 1.5, 'bounds': [0, 3], 'precision': 1e-1}}
+        i1 = Individual([1, 2, 2])
+        i2 = Individual([3, 2, 1])
+        sbx = SimulatedBinaryCrossover(parameters, 10)
+        print(sbx.run([i1, i2]))
+
+
+if __name__ == '__main__':
+    unittest.main()
