7a1fd0f2b7537205310331f2c427499b1f187c24
Petr Kropík <pkropik@kte.zcu.cz>
2019-01-22 18:41:18 +0100

    reading data from server on demand works, still from csv, next step will be from db
    Former-commit-id: c0691f96eca1b6878d198fb729f45b69e1b39044 [formerly 0c726d0aac48646b9ca2731119a4f0aa66d1cc0e [formerly 0ec35e87cdfd6fc851ad3158f2f221a32008c7e1]]
    Former-commit-id: d46af11769167ced18caa7496a925404b872e0db
    Former-commit-id: 07554892e2ec74257682fe7e3c5290894e6bbf60

--- artap_server/artap_server.py
+++ artap_server/artap_server.py
@@ -1,8 +1,3 @@
-import json
-from textwrap import dedent as d
-
-import flask
-
 import dash
 import dash_core_components as dashComp
 import dash_html_components as dashHtml
@@ -10,14 +5,14 @@ from dash.dependencies import Input, Output
 import plotly.graph_objs as grObj
 
 import numpy as np
-
+import pandas as pd
+import os
 
 x = np.linspace(0, 5, 50)
 y = np.sin(x)
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__)
 
 styles = {
     'pre': {
@@ -30,7 +25,7 @@ styles = {
 
 app.layout = dashHtml.Div([
     dashComp.Graph(
-        id='basic-interactions',
+        id='live-update-graph',
         figure={
             'data': [grObj.Scatter(
                 {
@@ -41,108 +36,40 @@ app.layout = dashHtml.Div([
             )]
         }
     ),
-
-    dashHtml.Div(className='row', children=[
-        dashHtml.Div([
-            dashComp.Markdown(d("""
-                **Hover Data**
-
-                Mouse over values in the graph.
-            """)),
-            dashHtml.Pre(id='hover-data', style=styles['pre'])
-        ], className='three columns'),
-
-        dashHtml.Div([
-            dashComp.Markdown(d("""
-                **Click Data**
-
-                Click on points in the graph.
-            """)),
-            dashHtml.Pre(id='click-data', style=styles['pre']),
-        ], className='three columns'),
-
-        dashHtml.Div([
-            dashComp.Markdown(d("""
-                **Selection Data**
-
-                Choose the lasso or rectangle tool in the graph's menu
-                bar and then select points in the graph.
-            """)),
-            dashHtml.Pre(id='selected-data', style=styles['pre']),
-        ], className='three columns'),
-
-        dashHtml.Div([
-            dashComp.Markdown(d("""
-                **Zoom and Relayout Data**
-
-                Click and drag on the graph to zoom or click on the zoom
-                buttons in the graph's menu bar.
-                Clicking on legend items will also fire
-                this event.
-            """)),
-            dashHtml.Pre(id='relayout-data', style=styles['pre']),
-        ], className='three columns')
-    ])
+    dashComp.Interval(
+        id='interval-component',
+        interval=1 * 1000,  # in milliseconds
+        n_intervals=0
+    )
 ])
 
 
-@app.callback(
-    Output('hover-data', 'children'),
-    [Input('basic-interactions', 'hoverData')])
-def display_hover_data(hoverData):
-    return json.dumps(hoverData, indent=2)
+# Multiple components can update everytime interval gets fired.
+@app.callback(Output('live-update-graph', 'figure'),
+              [Input('interval-component', 'n_intervals')])
+def update_graph_live(n):
+    x = np.linspace(0, 5, 50)
+    y = np.sin(x * n) / n
+    print(n)
+    dummy_data = pd.read_csv('testdata.csv')
+    print(dummy_data)
 
-
-@app.callback(
-    Output('click-data', 'children'),
-    [Input('basic-interactions', 'clickData')])
-def display_click_data(clickData):
-    return json.dumps(clickData, indent=2)
-
-
-@app.callback(
-    Output('selected-data', 'children'),
-    [Input('basic-interactions', 'selectedData')])
-def display_selected_data(selectedData):
-    return json.dumps(selectedData, indent=2)
-
-
-@app.callback(
-    Output('relayout-data', 'children'),
-    [Input('basic-interactions', 'relayoutData')])
-def display_selected_data(relayoutData):
-    return json.dumps(relayoutData, indent=2)
-
-@app.server.route('/foo', methods=['POST'])
-def foo():
-    if not flask.request.json:
-        flask.abort(400)
-    print(flask.request.json)
-    return json.dumps(flask.request.json)
-
-
-# Create a login route
-@app.server.route('/custom-auth/login', methods=['POST'])
-def route_login():
-    data = flask.request.form
-    username = data.get('username')
-    password = data.get('password')
-
-    if not username or not password:
-        flask.abort(401)
-
-    # actual implementation should verify the password.
-    # Recommended to only keep a hash in database and use something like
-    # bcrypt to encrypt the password and check the hashed results.
-
-    # Return a redirect with
-    rep = flask.redirect(_app_route)
-
-    # Here we just store the given username in a cookie.
-    # Actual session cookies should be signed or use a JWT token.
-    rep.set_cookie('custom-auth-session', username)
-    return rep
+    fig = {
+            'data': [grObj.Scatter(
+                {
+                    'x': x,
+                    'y': y,
+                    'name': 'Trace 1'
+                }
+            )],
+            'layout': grObj.Layout(
+                xaxis={'title': dummy_data.to_string()},
+                yaxis={'title': dummy_data.to_string()}
+            )
+        }
+    return fig
 
 
 if __name__ == '__main__':
+    #app.run_server(debug=False, host='147.228.96.138', port=8050)
     app.run_server(debug=True)
