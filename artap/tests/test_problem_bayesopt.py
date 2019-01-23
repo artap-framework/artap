851efb6ee8bd259624a59e6b3f1f45f5cfe37649
David Pánek <panek50@kte.zcu.cz>
2019-01-23 09:24:59 +0100

    some tests are working after refactoring
    Former-commit-id: 97ba688adeef87af52227240d9ccd932d13c8b6d [formerly fdd0f008f86afaa92571d7e58b326a458d1dcb89 [formerly 498dfb5d3591a738c9c6505010371f58f34eece5]]
    Former-commit-id: 14e0de81a91216f97fb5bc1ac143ee5c0ee5890d
    Former-commit-id: 25299288ce938deb1746063a2bc011a05aa2a9ae

--- artap/tests/test_problem_bayesopt.py
+++ artap/tests/test_problem_bayesopt.py
@@ -1,54 +1,54 @@
-# import os
-# import unittest
-#
-# from artap.problem import Problem
-# from artap.algorithm_bayesopt import BayesOptSerial, BayesOptParallel
-#
-# from artap.results import Results
-# from artap.datastore import DummyDataStore
-# from artap.benchmark_functions import Booth
-#
-#
-# class MyProblem(Problem):
-#     """ Describe simple one objective optimization problem. """
-#     def __init__(self, name):
-#         parameters = {'x_1': {'initial_value': 2.5, 'bounds': [-30, 30], 'precision': 1e-1},
-#                       'x_2': {'initial_value': 1.5, 'bounds': [-30, 30], 'precision': 1e-1}}
-#         costs = ['F']
-#
-#         super().__init__(name, parameters, costs, data_store=DummyDataStore(self))
-#         self.options['max_processes'] = 1
-#
-#     def evaluate(self, x):
-#         return [Booth.eval(x)]
-#
-#
-# class TestBayesOptOptimization(unittest.TestCase):
-#     """ Tests simple one objective optimization problem."""
-#
-#     def xtest_local_problem_bayesopt_parallel(self):
-#         problem = MyProblem("TestBayesOptParallel")
-#         algorithm = BayesOptParallel(problem)
-#         algorithm.options['verbose_level'] = 0
-#         algorithm.options['n_iterations'] = 100
-#         algorithm.run()
-#         # TODO - multiprocess test
-#
-#         # results = Results(problem)
-#         # optimum = results.find_minimum(name='F')
-#         # self.assertAlmostEqual(optimum, 0, places=2)
-#
-#     def test_local_problem_bayesopt_serial(self):
-#         problem = MyProblem("TestBayesOptSerial")
-#         algorithm = BayesOptSerial(problem)
-#         algorithm.options['verbose_level'] = 0
-#         algorithm.options['n_iterations'] = 200
-#         algorithm.run()
-#
-#         results = Results(problem)
-#         optimum = results.find_minimum(name='F')
-#         self.assertAlmostEqual(optimum, 0, places=2)
-#
-#
-# if __name__ == '__main__':
-#     unittest.main()
+import os
+import unittest
+
+from artap.problem import Problem
+from artap.algorithm_bayesopt import BayesOptSerial, BayesOptParallel
+
+from artap.results import Results
+from artap.datastore import DummyDataStore
+from artap.benchmark_functions import Booth
+
+
+class MyProblem(Problem):
+    """ Describe simple one objective optimization problem. """
+    def __init__(self, name):
+        parameters = {'x_1': {'initial_value': 2.5, 'bounds': [-30, 30], 'precision': 1e-1},
+                      'x_2': {'initial_value': 1.5, 'bounds': [-30, 30], 'precision': 1e-1}}
+        costs = ['F']
+
+        super().__init__(name, parameters, costs, data_store=DummyDataStore(self))
+        self.options['max_processes'] = 1
+
+    def evaluate(self, x):
+        return [Booth.eval(x)]
+
+
+class TestBayesOptOptimization(unittest.TestCase):
+    """ Tests simple one objective optimization problem."""
+
+    def xtest_local_problem_bayesopt_parallel(self):
+        problem = MyProblem("TestBayesOptParallel")
+        algorithm = BayesOptParallel(problem)
+        algorithm.options['verbose_level'] = 0
+        algorithm.options['n_iterations'] = 100
+        algorithm.run()
+        # TODO - multiprocess test
+
+        # results = Results(problem)
+        # optimum = results.find_minimum(name='F')
+        # self.assertAlmostEqual(optimum, 0, places=2)
+
+    def test_local_problem_bayesopt_serial(self):
+        problem = MyProblem("TestBayesOptSerial")
+        algorithm = BayesOptSerial(problem)
+        algorithm.options['verbose_level'] = 0
+        algorithm.options['n_iterations'] = 200
+        algorithm.run()
+
+        results = Results(problem)
+        optimum = results.find_minimum(name='F')
+        self.assertAlmostEqual(optimum, 0, places=2)
+
+
+if __name__ == '__main__':
+    unittest.main()
