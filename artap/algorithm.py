db04d4532ee7a367423b8a07b37ab8c8343bec88
Pavel Karban <pkarban@gmail.com>
2019-02-02 13:22:30 +0100

    nsga sbx works
    Former-commit-id: 2ba48ec2c0f83b4d3167f7c987765b75dbbb0df0 [formerly 4a9eab9f6ba24a86b975db95b48f39f955e47d15 [formerly c06ad17535772965da1fc86d0153327fdb319d86]]
    Former-commit-id: 5cc4029f468b42b1ca6542162bfed2132c2406d5
    Former-commit-id: e647dbae975c907667c1c3e753781e252e901b41

--- artap/algorithm.py
+++ artap/algorithm.py
@@ -1,8 +1,6 @@
 from .problem import Problem
 from .population import Population
-from .individual import Individual
 from .utils import ConfigDictionary
-from .utils import VectorAndNumbers
 from .job import JobSimple, JobQueue
 
 from multiprocessing import Process, Manager, Queue
@@ -79,46 +77,6 @@ class Algorithm(metaclass=ABCMeta):
         queue.join_thread()
 
         return individuals
-
-
-class Sensitivity(Algorithm):
-    def __init__(self, problem, parameters, name='Sensitivity analysis'):
-        self.parameters = parameters
-        super().__init__(problem, name)
-
-    def run(self):
-        parameters = []
-        for parameter in self.problem.parameters.items():
-            parameters.append(float(parameter[1]['initial_value']))
-
-        for parameter_name in self.parameters:
-            parameter_values = []
-            if len(self.problem.data_store.populations) > 1:
-                self.population = Population()
-
-            index = 0
-            selected_parameter = None
-            for parameter in self.problem.parameters.items():
-                if parameter[0] == parameter_name:
-                    selected_parameter = parameter
-                    break
-                index += 1
-
-            individuals = []
-            for i in range(self.options['max_population_size']):
-                value = VectorAndNumbers.gen_number(selected_parameter[1]['bounds'], selected_parameter[1]['precision'],
-                                              'normal')
-                parameters[index] = value
-                parameter_values.append(value)
-                individual = Individual(parameters.copy())
-                individuals.append(individual)
-
-            self.population.individuals = individuals
-            self.evaluate_population()
-            costs = []
-            # TODO: Make also for multi-objective
-            for individual in self.population.individuals:
-                costs.append(individual.costs)
 
 
 class EvalAll(Algorithm):
