4e6413f1412af56905b644cbe25127ad1b5a21e4
Petr Kropík <pkropik@kte.zcu.cz>
2019-02-03 00:10:09 +0100

    artap server as class, testing version
    Former-commit-id: ed2fa4c92beb3db89f7f5b72e4d25a5c31a7d545

--- artap_server/artap_server.py
+++ artap_server/artap_server.py
@@ -4,72 +4,126 @@ import dash_html_components as dashHtml
 from dash.dependencies import Input, Output
 import plotly.graph_objs as grObj
 
+import webbrowser
+import socket
+import threading
+from threading import Thread
+
 import numpy as np
 import pandas as pd
-import os
-
-x = np.linspace(0, 5, 500)
-y = np.sin(x)
 
 
-app = dash.Dash(__name__)
+class ArtapServer(Thread):
 
-styles = {
-    'pre': {
-        'border': 'thin lightgrey solid',
-        'overflowX': 'scroll'
-    }
-}
+    def __init__(self, local_host=True, port=8050, debug_mode=False):
+        Thread.__init__(self)
 
-# app.layout = dashHtml.Div()
+        if local_host:
+            self.url = '127.0.0.1'
+        else:
+            self.url = self.get_host_ip()
 
-app.layout = dashHtml.Div([
-    dashComp.Graph(
-        id='live-update-graph',
-        figure={
-            'data': [grObj.Scatter(
-                {
-                    'x': x,
-                    'y': y,
-                    'name': 'Trace 1',
+        self.port = port
+        self.debug_mode = debug_mode
+
+        self.__x = np.linspace(0, 5, 500)
+        self.__y = np.sin(self.__x)
+
+        self.__dash_app = dash.Dash(__name__)
+
+        self.__dash_app.layout = dashHtml.Div([
+            dashComp.Graph(
+                id='live-update-graph',
+                figure={
+                    'data': [grObj.Scatter(
+                        {
+                            'x': self.x,
+                            'y': self.y,
+                            'name': 'Trace 1',
+                        }
+                    )]
                 }
-            )]
-        }
-    ),
-    dashComp.Interval(
-        id='interval-component',
-        interval=1 * 1000,  # in milliseconds
-        n_intervals=0
-    )
-])
-
-
-# Multiple components can update everytime interval gets fired.
-@app.callback(Output('live-update-graph', 'figure'),
-              [Input('interval-component', 'n_intervals')])
-def update_graph_live(n):
-    x = np.linspace(0, 5, 500)
-    y = np.sin(x * n) / n
-    print(n)
-    dummy_data = pd.read_csv('testdata.csv')
-    print(dummy_data)
-
-    fig = {
-            'data': [grObj.Scatter(
-                {
-                    'x': x,
-                    'y': y,
-                    'name': 'Trace 1'
-                }
-            )],
-            'layout': grObj.Layout(
-                xaxis={'title': dummy_data.to_string()},
-                yaxis={'title': dummy_data.to_string()}
+            ),
+            dashComp.Interval(
+                id='interval-component',
+                interval=1 * 1000,  # in milliseconds
+                n_intervals=0
             )
-        }
-    return fig
+        ])
+
+        # Multiple components can update everytime interval gets fired.
+        @self.dash_app.callback(Output('live-update-graph', 'figure'),
+                      [Input('interval-component', 'n_intervals')])
+        def update_graph_live(n):
+            self.y = np.sin(self.x * n) / n
+            print(n)
+            dummy_data = pd.read_csv('testdata.csv')
+            print(dummy_data)
+
+            fig = {
+                'data': [grObj.Scatter(
+                    {
+                        'x': self.x,
+                        'y': self.y,
+                        'name': 'Trace 1'
+                    }
+                )],
+                'layout': grObj.Layout(
+                    xaxis={'title': dummy_data.to_string()},
+                    yaxis={'title': dummy_data.to_string()}
+                )
+            }
+            return fig
+
+    @property
+    def x(self):
+        """ 'x' property."""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        """ 'y' property."""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
+
+    @property
+    def dash_app(self):
+        """ 'dash_app' property."""
+        return self.__dash_app
+
+    def get_host_ip(self):
+        try:
+            return socket.gethostbyname(socket.gethostname())
+        except:
+            return None
+
+    def run(self):
+        self.dash_app.run_server(debug=self.debug_mode, host=self.url, port=self.port)
+
+    def run_server(self):
+        self.setDaemon(daemonic=True)
+        self.start()
+
+        '''
+        if open_viewer:
+            webbrowser.open(self.url, new=2, autoraise=True)
+        '''
 
 
 if __name__ == '__main__':
-    #app.run_server(debug=False, host='147.228.96.138', port=8050)
-    app.run_server(debug=True)
+    artap_server = ArtapServer(debug_mode=False)
+
+    artap_server.dash_app.run_server(debug=artap_server.debug_mode, host=artap_server.url, port=artap_server.port)
+
+    #artap_server.run_server()
+    #artap_server.setDaemon(True)
+    #artap_server.start()
+
+    #webbrowser.open('127.0.0.1', new=2, autoraise=True)
