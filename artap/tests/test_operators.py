b14bfa2083e2307af08d512ba3a37d526f03392b
Pavel Karban <pkarban@gmail.com>
2019-02-02 13:28:28 +0100

    test algorithm fixed
    Former-commit-id: 946c94ac4c7b3c6635b7c7df98c54df5f1aecfa4 [formerly 08775c95f98c5d5e6efcd81b79f968b88fce41e8 [formerly c9497a92f3aad4af86918ac72bb776d8b62248e6]]
    Former-commit-id: a574d4d57e13a2529d5d60efd74cb0eb88d47b96
    Former-commit-id: 2dd31102e6743de0f213d302f06b8e55b08a79a5

--- artap/tests/test_operators.py
+++ artap/tests/test_operators.py
@@ -1,20 +1,27 @@
-from artap.operators import SimulatedBinaryCrossover
+from artap.operators import SimulatedBinaryCrossover, SimpleCrossover
 from artap.individual import Individual
 
 import unittest
 
 
-class TestSensitivity(unittest.TestCase):
-    """ Tests simple one objective optimization problem."""
+class TestCrossover(unittest.TestCase):
+    """ Tests crossover."""
 
-    def test_local_problem(self):
-        parameters = {'x_1': {'initial_value': 2.5, 'bounds': [0, 5], 'precision': 1e-1},
-                      'x_2': {'initial_value': 1.5, 'bounds': [0, 3], 'precision': 1e-1}}
-        i1 = Individual([1, 2, 2])
-        i2 = Individual([3, 2, 1])
-        parents = [i1, i2]
-        sbx = SimulatedBinaryCrossover(parameters, 10)
-        offsprings = sbx.run(parents)
+    def setUp(self):
+        self.parameters = {'x_1': {'initial_value': 2.5, 'bounds': [0, 5], 'precision': 1e-1},
+                          'x_2': {'initial_value': 1.5, 'bounds': [0, 3], 'precision': 1e-1}}
+
+        self.i1 = Individual([1, 2, 2])
+        self.i2 = Individual([3, 2, 1])
+
+    def test_sbx(self):
+        sbx = SimulatedBinaryCrossover(10)
+        offsprings = sbx.cross(self.parameters, self.i1, self.i2)
+        self.assertEqual(len(offsprings), 2)
+
+    def test_simple_crossover(self):
+        sbx = SimpleCrossover()
+        offsprings = sbx.cross(self.parameters, self.i1, self.i2)
         self.assertEqual(len(offsprings), 2)
 
 
