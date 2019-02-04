d4d1599d81d5db652e3e17cef36c5b6e727e5103
Pavel Karban <pkarban@gmail.com>
2019-02-04 13:54:34 +0100

    minor improvements (parameters in constructor)
    Former-commit-id: 0779e0be401e54d920b8f019e673f0b81cfdd703

--- artap/tests/test_operators.py
+++ artap/tests/test_operators.py
@@ -1,4 +1,4 @@
-from artap.operators import SimulatedBinaryCrossover, SimpleCrossover
+from artap.operators import RandomGeneration, SimpleMutation, SimulatedBinaryCrossover, SimpleCrossover
 from artap.individual import Individual
 
 import unittest
@@ -9,19 +9,34 @@ class TestCrossover(unittest.TestCase):
 
     def setUp(self):
         self.parameters = {'x_1': {'initial_value': 2.5, 'bounds': [0, 5], 'precision': 1e-1},
-                          'x_2': {'initial_value': 1.5, 'bounds': [0, 3], 'precision': 1e-1}}
+                           'x_2': {'initial_value': 1.5, 'bounds': [0, 3], 'precision': 1e-1}}
 
         self.i1 = Individual([1, 2, 2])
         self.i2 = Individual([3, 2, 1])
 
+    def test_random_generator(self):
+        gen = RandomGeneration(self.parameters)
+        individuals = gen.generate(2)
+        self.assertTrue(self.parameters['x_1']['bounds'][0] <= individuals[0].vector[0] <= self.parameters['x_1']['bounds'][1] and
+                        self.parameters['x_2']['bounds'][0] <= individuals[0].vector[1] <= self.parameters['x_2']['bounds'][1] and
+                        self.parameters['x_1']['bounds'][0] <= individuals[1].vector[0] <= self.parameters['x_1']['bounds'][1] and
+                        self.parameters['x_2']['bounds'][0] <= individuals[1].vector[1] <= self.parameters['x_2']['bounds'][1])
+
+    def test_simple_mutation(self):
+        sbx = SimpleMutation(self.parameters, 0.7)
+        individual = sbx.mutate(self.i1)
+        self.assertTrue(
+            self.parameters['x_1']['bounds'][0] <= individual.vector[0] <= self.parameters['x_1']['bounds'][1] and
+            self.parameters['x_2']['bounds'][0] <= individual.vector[1] <= self.parameters['x_2']['bounds'][1])
+
     def test_sbx(self):
-        sbx = SimulatedBinaryCrossover(10)
-        offsprings = sbx.cross(self.parameters, self.i1, self.i2)
+        sbx = SimulatedBinaryCrossover(self.parameters, 10)
+        offsprings = sbx.cross(self.i1, self.i2)
         self.assertEqual(len(offsprings), 2)
 
     def test_simple_crossover(self):
-        sbx = SimpleCrossover()
-        offsprings = sbx.cross(self.parameters, self.i1, self.i2)
+        sbx = SimpleCrossover(self.parameters)
+        offsprings = sbx.cross(self.i1, self.i2)
         self.assertEqual(len(offsprings), 2)
 
 
