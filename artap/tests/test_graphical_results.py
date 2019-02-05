6b9e9061a2c89b7764a4ec17abec0fd1be58f626
Pavel Karban <pkarban@gmail.com>
2019-02-05 08:52:04 +0100

    fixes, artap_server temporarily disabled
    Former-commit-id: 24c3d5f6e4a5aecafea05a2737543eb4ac2c21f6

--- artap/tests/test_graphical_results.py
+++ artap/tests/test_graphical_results.py
@@ -9,6 +9,7 @@ from artap.results import GraphicalResults
 from skimage.io import imread
 from skimage.measure import compare_mse as mse
 
+
 class TestDataStore(unittest.TestCase):
     def setUp(self):
         self.working_dir = tempfile.mkdtemp()
@@ -21,26 +22,28 @@ class TestDataStore(unittest.TestCase):
         self.results = GraphicalResults(problem)
 
     def tearDown(self):
-        rmtree(self.working_dir)
+        # rmtree(self.working_dir)
         pass
 
     def test_scatter(self):
-        self.results.plot_scatter('F_1', 'F_2', extension="png")
+        self.results.plot_scatter('F_1', 'F_2', filename=self.working_dir + os.sep + "scatter.png")
 
         original = imread("." + os.sep + "workspace" + os.sep + "common_data" + os.sep + "scatter.png")
         picture = imread(self.working_dir + os.sep + "scatter.png")
 
         mssim = mse(original, picture)
-        self.assertLess(mssim, 10)
+        # print("scatter: {}".format(mssim))
+        self.assertLess(mssim, 2)
 
     def test_individuals(self):
-        self.results.plot_individuals('F_1', extension="png")
+        self.results.plot_individuals('F_1', filename=self.working_dir + os.sep + "individuals.png")
 
         original = imread("." + os.sep + "workspace" + os.sep + "common_data" + os.sep + "individuals.png")
         picture = imread(self.working_dir + os.sep + "individuals.png")
 
         mssim = mse(original, picture)
-        self.assertLess(mssim, 10)
+        # print("individuals: {}".format(mssim))
+        self.assertLess(mssim, 2)
 
 if __name__ == '__main__':
     unittest.main()
