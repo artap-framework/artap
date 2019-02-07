315b31dac6ea53a1da3afeb773699a74cb89d2c7
David Pánek <panek50@kte.zcu.cz>
2019-02-07 14:10:23 +0100

    work on PSO
    Former-commit-id: 524b4e9de95977d0713fe90bbd384811f04c7588

--- artap/tests/test_problem_swarm.py
+++ artap/tests/test_problem_swarm.py
@@ -1,16 +1,17 @@
 import unittest
 from artap.problem import Problem
-from artap.datastore import DummyDataStore
+# from artap.datastore import DummyDataStore
 from artap.algorithm_swarm import PSO
 from artap.benchmark_functions import BinhAndKorn
+# from artap.results import GraphicalResults
 
 
 class PSORosenbrock(Problem):
     """ Search the optimal value of the Rosenbrock function in 2d"""
 
     def __init__(self, name):
-        parameters = {'x_1': {'initial_value': 2.5, 'bounds': [0, 5], 'precision': 1e-7},
-                      'x_2': {'initial_value': 2.5, 'bounds': [0, 5], 'precision': 1e-7}}
+        parameters = {'x_1': {'initial_value': 2.5, 'bounds': [0, 5]},
+                      'x_2': {'initial_value': 2.5, 'bounds': [0, 3]}}
         costs = ['F_1', 'F_2']
 
         super().__init__(name, parameters, costs)
@@ -24,12 +25,15 @@ class PSORosenbrock(Problem):
 class TestPSOptimization(unittest.TestCase):
     """ Tests simple one objective optimization problem."""
 
-    def disabled_test_local_problem_pso(self):
+    def test_local_problem_pso(self):
         problem = PSORosenbrock("PSORosenbrock")
         algorithm = PSO(problem)
-        algorithm.options['max_population_number'] = 20
+        algorithm.options['max_population_number'] = 10
         algorithm.options['max_population_size'] = 100
         algorithm.run()
+        # results = GraphicalResults(problem)
+        # results.plot_scatter_vectors('x_1', 'x_2', filename="/tmp/scatter.pdf", population_number=10)
+        # results.plot_scatter('F_1', 'F_2', filename="/tmp/scatter.pdf", population_number=2)
 
 
 if __name__ == '__main__':
