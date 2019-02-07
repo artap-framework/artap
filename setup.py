8811c37dd62ee77cc08584618d1d4568e5a45543
Tamás <orosz.tamas@yahoo.com>
2019-02-07 12:33:08 +0100

    very first version of setup.py and pip install
    Former-commit-id: 9035d08898c1eb96c796001c7b5bff2faf70f1f9

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-your-username",
-    version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    name="Artap",
+    version="0.10",
+    author="Artap Team",
+    author_email="artap.team@gmail.com",
+    description="Platform for robust design optimization",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/pypa/sampleproject",
