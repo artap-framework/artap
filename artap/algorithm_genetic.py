87ee635bebf8c099d893e6b432f0d054dbaa40f7
David Pánek <panek50@kte.zcu.cz>
2019-02-08 11:46:44 +0100

    Work on eps_moea
    Former-commit-id: 562c9261a1183477ff35361f2a1d8af7466c8c5c

--- artap/algorithm_genetic.py
+++ artap/algorithm_genetic.py
@@ -1,7 +1,8 @@
 from .problem import Problem
 from .algorithm import Algorithm
 from .population import Population
-from .operators import RandomGeneration, SimpleCrossover, SimulatedBinaryCrossover, SimpleMutation, PmMutation, TournamentSelection, Selection
+from .operators import RandomGeneration, SimpleCrossover, SimulatedBinaryCrossover, SimpleMutation, \
+    PmMutation, TournamentSelection, Selection, ParetoDominance
 import time
 import sys
 
@@ -75,6 +76,67 @@ class GeneticAlgorithm(GeneralEvolutionaryAlgorithm):
 
 
 class NSGAII(GeneticAlgorithm):
+
+    def __init__(self, problem: Problem, name="NSGA_II Evolutionary Algorithm"):
+        super().__init__(problem, name)
+
+        self.options.declare(name='prob_cross', default=0.6, lower=0,
+                             desc='prob_cross')
+        self.options.declare(name='prob_mutation', default=0.2, lower=0,
+                             desc='prob_mutation')
+
+    def run(self):
+        # set class
+        # self.crossover = SimpleCrossover(self.problem.parameters, self.options['prob_cross'])
+        self.crossover = SimulatedBinaryCrossover(self.problem.parameters, self.options['prob_cross'])
+        # self.mutator = SimpleMutation(self.problem.parameters, self.options['prob_mutation'])
+        self.mutator = PmMutation(self.problem.parameters, self.options['prob_mutation'])
+        self.selector = TournamentSelection(self.problem.parameters)
+
+        # create initial population and evaluate individuals
+        population = self.gen_initial_population()
+        # non-dominated sort of individuals
+        self.selector.non_dominated_sort(population.individuals)
+        self.selector.crowding_distance(population.individuals)
+        # write to data store
+        self.problem.data_store.write_population(population)
+
+        t_s = time.time()
+        self.problem.logger.info(
+            "NSGA_II: {}/{}".format(self.options['max_population_number'], self.options['max_population_size']))
+
+        # optimization
+        for it in range(self.options['max_population_number']):
+            # generate new offsprings
+            offsprings = self.generate(population.individuals)
+            # evaluate the offsprings
+            offsprings = self.evaluate(offsprings)
+
+            # if (self.options['calculate_gradients'] is True) and population.number > 20:
+            #     population.evaluate_gradients()
+
+            # add the parents to the offsprings
+            offsprings.extend(population.individuals)
+
+            # non-dominated truncate on the guys
+            self.selector.non_dominated_sort(offsprings)
+            self.selector.crowding_distance(offsprings)
+
+            # sort offsprings
+            parents = sorted(offsprings, key=lambda x: (x.front_number, -x.crowding_distance))
+
+            # truncate
+            offsprings = parents[:self.options['max_population_size']]
+
+            # write population
+            population = Population(offsprings)
+            self.problem.data_store.write_population(population)
+
+        t = time.time() - t_s
+        self.problem.logger.info("NSGA_II: elapsed time: {} s".format(t))
+
+
+class EpsMOEA(GeneticAlgorithm):
 
     def __init__(self, problem: Problem, name="NSGA_II Evolutionary Algorithm"):
         super().__init__(problem, name)
