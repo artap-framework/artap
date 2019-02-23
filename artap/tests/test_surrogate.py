17853a39d4617fea1afffa1eb5ecebfde8b14056
Pavel Karban <pkarban@gmail.com>
2019-02-23 20:25:43 +0100

    surrogate progress
    Former-commit-id: cad2f639719ef89fe85094d7af8c033b0b679707 [formerly 6d3a2a41fa7429ed79cede87de91704352ae6c77 [formerly 8804bdaf693967d099dd9578488f0a012e678038]]
    Former-commit-id: 434df759ba9d72d76d92694d147969c3e3e2826c
    Former-commit-id: 16fb8da6fc9496ab5cf66fc99ba4127ffa26273e

--- artap/tests/test_surrogate.py
+++ artap/tests/test_surrogate.py
@@ -80,53 +80,16 @@ class TestSurrogate(unittest.TestCase):
         problem.surrogate.regressor = GaussianProcessRegressor(kernel=kernel)
 
         self.check_one(problem)
-        """
-        kernels = [1.0 * RBF(length_scale=1.0, length_scale_bounds=(1e-1, 10.0)),
-                   1.0 * RationalQuadratic(length_scale=1.0, alpha=0.1),
-                   1.0 * ExpSineSquared(length_scale=1.0, periodicity=3.0,
-                                        length_scale_bounds=(0.1, 10.0),
-                                        periodicity_bounds=(1.0, 10.0)),
-                   ConstantKernel(0.1, (0.01, 10.0))
-                   * (DotProduct(sigma_0=1.0, sigma_0_bounds=(0.1, 10.0)) ** 2),
-                   1.0 * Matern(length_scale=1.0, length_scale_bounds=(1e-1, 10.0),
-                                nu=1.5)]
 
-        for kernel in kernels:
-            print(kernel)
-            problem.surrogate = SurrogateModelGaussianProcess(problem)
-            problem.surrogate.options['sigma_threshold'] = 0.05
-            problem.surrogate.options['train_step'] = 7
-            problem.surrogate.regressor = GaussianProcessRegressor(kernel=kernel)
-
-            # train
-            for val in [1., 2., 3., 5., 6., 7., 8.]:
-                problem.surrogate.evaluate([val])
-
-            x_ref = [4.9]
-            # eval reference
-            value_problem = problem.evaluate(x_ref)[0]
-            # eval surrogate
-            value_surrogate = problem.surrogate.evaluate(x_ref)[0]
-
-            problem.logger.info(
-                "surrogate.counter: evaluation = {}, prediction = {}".format(problem.surrogate.eval_counter,
-                                                                             problem.surrogate.predict_counter))
-            problem.logger.info(
-                "surrogate.value: evaluation = {}, prediction = {}, difference = {}".format(value_problem,
-                                                                                            value_surrogate, math.fabs(
-                        value_problem - value_surrogate)))
-            """
-
-    def xtest_gaussian_process_two(self):
+    def test_gaussian_process_two(self):
         problem = MyProblemBooth("MyProblemBooth")
         problem.surrogate = SurrogateModelGaussianProcess(problem)
         # set custom regressor
-        # kernel = ConstantKernel(1.0, (1e-3, 1e3)) * RBF(10, (1e-6, 3e2))
-        kernel = 1.0 * RBF(length_scale=100.0, length_scale_bounds=(1e-2, 1e3))
-        kernel = DotProduct() + WhiteKernel()
+
+        kernel = 1.0 * RationalQuadratic(length_scale=1.0, alpha=0.1)
         problem.surrogate.regressor = GaussianProcessRegressor(kernel=kernel)
         # set threshold
-        problem.surrogate.options['sigma_threshold'] = 0.1
+        problem.surrogate.options['sigma_threshold'] = 0.01
         problem.surrogate.options['train_step'] = 12
 
         # train
@@ -149,12 +112,64 @@ class TestSurrogate(unittest.TestCase):
         # eval surrogate
         value_surrogate = problem.surrogate.evaluate(x_ref)[0]
 
-        problem.logger.info("surrogate.counter: evaluation = {}, prediction = {}".format(problem.surrogate.eval_counter, problem.surrogate.predict_counter))
-        problem.logger.info("surrogate.value: evaluation = {}, prediction = {}, difference = {}".format(value_problem, value_surrogate, math.fabs( value_problem - value_surrogate)))
+        problem.logger.info("surrogate.counter: evaluation = {}, prediction = {}".format(problem.surrogate.eval_counter,
+                                                                                         problem.surrogate.predict_counter))
+        problem.logger.info("surrogate.value: evaluation = {}, prediction = {}, difference = {}".format(value_problem, value_surrogate,
+                                                                                                        math.fabs(value_problem - value_surrogate)))
 
         self.assertTrue(problem.surrogate.predict_counter > 0)
-        self.assertLess(math.fabs(value_problem - value_surrogate), 0.03)
+        self.assertLess(math.fabs(value_problem - value_surrogate), 0.01)
 
 
 if __name__ == '__main__':
     unittest.main()
+
+
+"""
+kernels = [1.0 * RBF(length_scale=1.0, length_scale_bounds=(1e-1, 10.0)),
+       1.0 * RationalQuadratic(length_scale=1.0, alpha=0.1),
+       1.0 * ExpSineSquared(length_scale=1.0, periodicity=3.0,
+                            length_scale_bounds=(0.1, 10.0),
+                            periodicity_bounds=(1.0, 10.0)),
+       ConstantKernel(0.1, (0.01, 10.0))
+       * (DotProduct(sigma_0=1.0, sigma_0_bounds=(0.1, 10.0)) ** 2),
+       1.0 * Matern(length_scale=1.0, length_scale_bounds=(1e-1, 10.0),
+                    nu=1.5)]
+
+for kernel in kernels:
+    print(kernel)
+
+    problem.surrogate = SurrogateModelGaussianProcess(problem)
+    problem.surrogate.regressor = GaussianProcessRegressor(kernel=kernel)
+    # set threshold
+    problem.surrogate.options['sigma_threshold'] = 0.1
+    problem.surrogate.options['train_step'] = 12
+
+    # train
+    problem.surrogate.evaluate([1.0, 3.1])
+    problem.surrogate.evaluate([1.3, 3.2])
+    problem.surrogate.evaluate([1.8, 2.7])
+    problem.surrogate.evaluate([0.9, 3.3])
+    problem.surrogate.evaluate([0.8, 3.1])
+    problem.surrogate.evaluate([1.4, 2.8])
+    problem.surrogate.evaluate([0.1, 3.0])
+    problem.surrogate.evaluate([0.2, 3.4])
+    problem.surrogate.evaluate([0.95, 3.03])
+    problem.surrogate.evaluate([0.98, 2.96])
+    problem.surrogate.evaluate([1.02, 2.98])
+    problem.surrogate.evaluate([1.01, 2.99])
+
+    x_ref = [1.01, 3.01]
+    # eval reference
+    value_problem = problem.evaluate(x_ref)[0]
+    # eval surrogate
+    value_surrogate = problem.surrogate.evaluate(x_ref)[0]
+
+    problem.logger.info(
+        "surrogate.counter: evaluation = {}, prediction = {}".format(problem.surrogate.eval_counter,
+                                                                     problem.surrogate.predict_counter))
+    problem.logger.info(
+        "surrogate.value: evaluation = {}, prediction = {}, difference = {}".format(value_problem,
+                                                                                    value_surrogate, math.fabs(
+                value_problem - value_surrogate)))
+"""
