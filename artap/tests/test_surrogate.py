cdb91da3802b43fed113d53ac2552333afbeceea
Pavel Karban <pkarban@gmail.com>
2019-02-23 20:18:12 +0100

    surrogate
    Former-commit-id: ce603902d13b07406e4baf04a740fa85a5b75ba5

--- artap/tests/test_surrogate.py
+++ artap/tests/test_surrogate.py
@@ -6,7 +6,7 @@ from artap.benchmark_functions import Booth
 from artap.surrogate import SurrogateModelEval, SurrogateModelGaussianProcess
 
 from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import RBF, Matern, ConstantKernel as C
+from sklearn.gaussian_process.kernels import RBF, Matern, DotProduct, WhiteKernel, ConstantKernel, RationalQuadratic, ExpSineSquared
 
 
 class MyProblemSin(Problem):
@@ -74,20 +74,60 @@ class TestSurrogate(unittest.TestCase):
     def test_gaussian_process_one(self):
         problem = MyProblemSin("MyProblemSin")
         problem.surrogate = SurrogateModelGaussianProcess(problem)
-        # default kernel
-        kernel = C(1.0, (1e-3, 1e3)) * RBF(10, (1e-6, 3e2))
-        problem.surrogate.regressor = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=9)
-        problem.surrogate.options['sigma_threshold'] = 0.05
+        problem.surrogate.options['sigma_threshold'] = 0.1
+
+        kernel = 1.0 * ExpSineSquared(length_scale=1.0, periodicity=3.0, length_scale_bounds=(0.1, 10.0), periodicity_bounds=(1.0, 10.0))
+        problem.surrogate.regressor = GaussianProcessRegressor(kernel=kernel)
 
         self.check_one(problem)
+        """
+        kernels = [1.0 * RBF(length_scale=1.0, length_scale_bounds=(1e-1, 10.0)),
+                   1.0 * RationalQuadratic(length_scale=1.0, alpha=0.1),
+                   1.0 * ExpSineSquared(length_scale=1.0, periodicity=3.0,
+                                        length_scale_bounds=(0.1, 10.0),
+                                        periodicity_bounds=(1.0, 10.0)),
+                   ConstantKernel(0.1, (0.01, 10.0))
+                   * (DotProduct(sigma_0=1.0, sigma_0_bounds=(0.1, 10.0)) ** 2),
+                   1.0 * Matern(length_scale=1.0, length_scale_bounds=(1e-1, 10.0),
+                                nu=1.5)]
+
+        for kernel in kernels:
+            print(kernel)
+            problem.surrogate = SurrogateModelGaussianProcess(problem)
+            problem.surrogate.options['sigma_threshold'] = 0.05
+            problem.surrogate.options['train_step'] = 7
+            problem.surrogate.regressor = GaussianProcessRegressor(kernel=kernel)
+
+            # train
+            for val in [1., 2., 3., 5., 6., 7., 8.]:
+                problem.surrogate.evaluate([val])
+
+            x_ref = [4.9]
+            # eval reference
+            value_problem = problem.evaluate(x_ref)[0]
+            # eval surrogate
+            value_surrogate = problem.surrogate.evaluate(x_ref)[0]
+
+            problem.logger.info(
+                "surrogate.counter: evaluation = {}, prediction = {}".format(problem.surrogate.eval_counter,
+                                                                             problem.surrogate.predict_counter))
+            problem.logger.info(
+                "surrogate.value: evaluation = {}, prediction = {}, difference = {}".format(value_problem,
+                                                                                            value_surrogate, math.fabs(
+                        value_problem - value_surrogate)))
+            """
 
     def xtest_gaussian_process_two(self):
         problem = MyProblemBooth("MyProblemBooth")
         problem.surrogate = SurrogateModelGaussianProcess(problem)
         # set custom regressor
-        # kernel = C(1.0, (1e-3, 1e3)) * RBF(10, (1e-6, 3e2))
+        # kernel = ConstantKernel(1.0, (1e-3, 1e3)) * RBF(10, (1e-6, 3e2))
         kernel = 1.0 * RBF(length_scale=100.0, length_scale_bounds=(1e-2, 1e3))
-        problem.surrogate.regressor = GaussianProcessRegressor(kernel=kernel, alpha=0.0)
+        kernel = DotProduct() + WhiteKernel()
+        problem.surrogate.regressor = GaussianProcessRegressor(kernel=kernel)
+        # set threshold
+        problem.surrogate.options['sigma_threshold'] = 0.1
+        problem.surrogate.options['train_step'] = 12
 
         # train
         problem.surrogate.evaluate([1.0, 3.1])
@@ -103,11 +143,7 @@ class TestSurrogate(unittest.TestCase):
         problem.surrogate.evaluate([1.02, 2.98])
         problem.surrogate.evaluate([1.01, 2.99])
 
-        # set threshold
-        problem.surrogate.options['sigma_threshold'] = 2.0
-        problem.surrogate.options['train_step'] = 12
-
-        x_ref = [1.01, 3.02]
+        x_ref = [1.01, 3.01]
         # eval reference
         value_problem = problem.evaluate(x_ref)[0]
         # eval surrogate
