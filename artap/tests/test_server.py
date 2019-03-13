e4b9e7d4b3039cdb35da4674c073439d39485ad1
Petr Kropík <pkropik@kte.zcu.cz>
2019-03-13 17:35:06 +0100

    change of tested problem in test_server.py
    Former-commit-id: 6f980560022a71fbce9da7941dbbfc0f9ddd6141

--- artap/tests/test_server.py
+++ artap/tests/test_server.py
@@ -4,12 +4,14 @@ import time
 from urllib.request import Request, urlopen
 from urllib.error import URLError, HTTPError
 
+from artap import NSGAII
 from artap.problem import Problem
 from artap.algorithm_scipy import ScipyOpt
 from artap.benchmark_functions import AckleyN2
 from artap.datastore import SqliteDataStore
 from artap.results import Results
 from artap.datastore import DummyDataStore
+from artap.tests.test_problem_nsga2 import AckleyN2Test
 
 
 class MyProblem(Problem):
@@ -50,21 +52,27 @@ class MyProblem(Problem):
             # print('Website is working fine')
             return True
 
+
 class TestSimpleOptimization(unittest.TestCase):
     """ Tests simple one objective optimization problem."""
 
     def test_local_problem(self):
-        #problem = MyProblem("TestSimpleOptimization")
-        #algorithm = ScipyOpt(problem)
-        #algorithm.options['algorithm'] = 'Nelder-Mead'
-        #algorithm.options['tol'] = 1e-4
+        #problem = MyProblem("TestAckleyN2")
+        #algorithm = NSGAII(problem)
+        #algorithm.options['max_population_number'] = 100
+        #algorithm.options['max_population_size'] = 50
+
         #time.sleep(3)
         #web_server_works = problem.test_server_works(problem.server.get_server_url())
+
         #algorithm.run()
+
         #problem.server.stop_server()
         web_server_works = True
         self.assertTrue(web_server_works, 'Web Server doesn\'t work.')
-
+        #b = Results(problem)
+        #optimum = b.find_minimum('F_1')  # Takes last cost function
+        #self.assertAlmostEqual(optimum, -200, 0)
 
 if __name__ == '__main__':
     unittest.main()
