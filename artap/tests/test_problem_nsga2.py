0db735ee8b13e08bad752c716e8b64a0b7901e18
Tamás <orosz.tamas@yahoo.com>
2019-06-28 15:45:18 +0200

    New algorithm - which is I hope eps-MOEA
    Former-commit-id: 8d45c2bfd4a4463e505b34dac538ffa959af8df5 [formerly 89e85c7437f98d0af8a6befec364e8ee9bd63fe6 [formerly acccbf72737d09d7d93f73d5deabbfd7da6f87c5]]
    Former-commit-id: 23d8e501677c3d82499a01fbe7d8b09dc2efeaac
    Former-commit-id: 36ea1392b8a68b7214489db600cd59c1cc22ad76

--- artap/tests/test_problem_nsga2.py
+++ artap/tests/test_problem_nsga2.py
@@ -5,6 +5,8 @@ from artap.benchmark_functions import BinhAndKorn, AckleyN2
 from artap.algorithm_genetic import NSGAII, EpsMOEA
 from artap.results import Results, GraphicalResults
 
+import optproblems as optp
+import optproblems.cec2005 as cec2005
 
 class BinhAndKornProblem(Problem):
     """ Describe simple one objective optimization problem. """
@@ -78,10 +80,59 @@ class TestAckleyN2(unittest.TestCase):
         b = Results(problem)
         optimum = b.find_minimum('F_1')  # Takes last cost function
         self.assertAlmostEqual(optimum.costs[0], -200, 0)
+#####
+#####
+class CEC2005_TEST_Problems(Problem):
+
+    def __init__(self, name, nr):
+
+        ub = 100
+        lb = -100
+
+        parameters = {'x_1': {'initial_value': 2.5, 'bounds': [ub, lb]},
+                      'x_2': {'initial_value': 2.5, 'bounds': [ub, lb]},
+                      'x_3': {'initial_value': 2.5, 'bounds': [ub, lb]},
+                      'x_4': {'initial_value': 2.5, 'bounds': [ub, lb]},
+                      'x_5': {'initial_value': 2.5, 'bounds': [ub, lb]}}
+
+        costs = ['F_1']
+
+        super().__init__(name, parameters, costs)
 
 
-class TestAckleyN2(unittest.TestCase):
+    def evaluate(self, x):
+
+        function = optp.cec2005.F1(5)
+        problem = optp.Problem(function, num_objectives = 1)
+        solutions = [optp.Individual(x)]
+
+        problem.batch_evaluate(solutions)
+
+        return [solutions[0].objective_values]
+
+
+class TestCEC2005(unittest.TestCase):
     """ Tests that the NSGA II algorithm can find the global optimum of a function."""
+
+    def test_local_problem(self):
+        problem = CEC2005_TEST_Problems("CEC2005", 1)
+        algorithm = NSGAII(problem)
+        algorithm.options['max_population_number'] = 100
+        algorithm.options['max_population_size'] = 100
+        #algorithm.options['max_population_number'] = 3
+        #algorithm.options['max_population_size'] = 3
+        algorithm.run()
+
+        b = Results(problem)
+        optimum = b.find_minimum('F_1')  # Takes last cost function
+
+
+        self.assertAlmostEqual(optimum.costs[0], -450, 0)
+
+#####
+#####
+class TestAckleyN2(unittest.TestCase):
+    """ Tests that the eps-moea algorithm can find the global optimum of a function."""
 
     def test_local_problem(self):
         problem = AckleyN2Test("TestAckleyN2")
