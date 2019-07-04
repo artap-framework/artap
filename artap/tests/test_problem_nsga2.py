0200927b881890daa6ff1e524e8aa3fad6c055a0
Tamás <orosz.tamas@yahoo.com>
2019-07-04 18:58:25 +0200

    - test tests optproblems should be added somewhere
    Former-commit-id: ea850ed8ba4916641a60dbea795aaa14ede16d1e

--- artap/tests/test_problem_nsga2.py
+++ artap/tests/test_problem_nsga2.py
@@ -5,9 +5,9 @@ from artap.benchmark_functions import BinhAndKorn, AckleyN2
 from artap.algorithm_genetic import NSGAII, EpsMOEA
 from artap.results import Results, GraphicalResults
 
-import optproblems as optp
-import optproblems.cec2005 as cec2005
-import optproblems.dtlz as dtlz
+#import optproblems as optp
+#import optproblems.cec2005 as cec2005
+#import optproblems.dtlz as dtlz
 
 from math import pi
 
@@ -147,99 +147,99 @@ class CEC2005_TEST_Problems(Problem):
 
 
 
-class TestCEC2005(unittest.TestCase):
-    """ Tests that the NSGA II algorithm can find the global optimum of a function."""
-
-    def run_test_problem(self, method, ub, lb, gen_nr):
-        problem = CEC2005_TEST_Problems("CEC2005",method, ub, lb)
-        algorithm = NSGAII(problem)
-        algorithm.options['max_population_number'] = gen_nr
-        algorithm.options['max_population_size'] = 100
-        algorithm.run()
-
-        b = Results(problem)
-        optimum = b.find_minimum('F_1')  # Takes last cost function
-        self.assertAlmostEqual(optimum.costs[0], method.bias, 0)
-
-    # unimodal problems
-
-    def test_shifted_sphere(self):
-        self.run_test_problem(optp.cec2005.F1, 100, -100, 100)
-
-    def test_shifted_double_sum(self):
-        self.run_test_problem(optp.cec2005.F2, 100, -100, 100)
-
-    # def test_shifted_rotated_elliptic(self): # wrong with platypus also
-    #     self.run_test_problem(optp.cec2005.F3, 100, -100, 10000)
-    #
-    #def test_shifted_schwefel(self): # wrong with platypus also
-    #    self.run_test_problem(optp.cec2005.F4, 100, -100, 10000)
-
-    def test_schwefel(self): # !
-        self.run_test_problem(optp.cec2005.F5, 100, -100, 100)
-
-    def test_shifted_rosenbrock(self):
-        self.run_test_problem(optp.cec2005.F6, 100, -100, 1000)
-
-    # multimodal problems
-    #def test_shifted_rot_griewank(self):
-    #    self.run_test_problem(optp.cec2005.F7, 100, -100, 10000)
-
-    #def test_shifted_rot_ackley(self):
-    #    self.run_test_problem(optp.cec2005.F8, 32, -32, 100)
-
-    def test_shifted_rastrigin(self):
-        self.run_test_problem(optp.cec2005.F9, 5, -5, 100)
-
-    def test_shifted_rot_rastrigin(self):
-        self.run_test_problem(optp.cec2005.F10, 5, -5, 1000)
-
-    def test_shifted_rot_weierstrass(self):
-        self.run_test_problem(optp.cec2005.F11, 0.5, -0.5, 100)
-
-    def test_fletcherpowell(self): #schwefel
-        self.run_test_problem(optp.cec2005.F12, pi, -pi, 100)
-
-    def test_f8f2(self):
-        self.run_test_problem(optp.cec2005.F13, 1, -3, 100)
-
-    def test_expanded_f6(self):
-        self.run_test_problem(optp.cec2005.F14, 100, -100, 100)
-
-    #####
-    def test_hybrid_composition(self):
-        self.run_test_problem(optp.cec2005.F15, 5, -5, 100)
-
-    def test_rotated_f15(self):
-        self.run_test_problem(optp.cec2005.F16, 5, -5, 100)
-
-    def test_f16_with_noise(self):
-        self.run_test_problem(optp.cec2005.F17, 5, -5, 250)
-
-    # def test_rot_hybrid_function(self):
-    #     self.run_test_problem(optp.cec2005.F18, 5, -5, 1000)
-    #
-    # def test_rot_hybrid_function_2(self):
-    #     self.run_test_problem(optp.cec2005.F19, 5, -5, 1000)
-    #
-    # def test_rot_hybrid_function_3(self):
-    #     self.run_test_problem(optp.cec2005.F20, 5, -5, 1000)
-    #
-    # def test_rot_hybrid_function_4(self):
-    #     self.run_test_problem(optp.cec2005.F21, 5, -5, 1000)
-    #
-    # def test_rot_hybrid_function_5(self):
-    #     self.run_test_problem(optp.cec2005.F22, 5, -5, 1000)
-    #
-    # def test_rot_hybrid_function_6(self):
-    #     self.run_test_problem(optp.cec2005.F23, 5, -5, 1000)
-    #
-    # def test_rot_hybrid_function_7(self):
-    #     self.run_test_problem(optp.cec2005.F24, 5, -5, 1000)
-    #
-    # def test_rot_hybrid_function_wo_bounds(self):
-    #     self.run_test_problem(optp.cec2005.F25, 100, -100, 100)
-
+# class TestCEC2005(unittest.TestCase):
+#     """ Tests that the NSGA II algorithm can find the global optimum of a function."""
+#
+#     def run_test_problem(self, method, ub, lb, gen_nr):
+#         problem = CEC2005_TEST_Problems("CEC2005",method, ub, lb)
+#         algorithm = NSGAII(problem)
+#         algorithm.options['max_population_number'] = gen_nr
+#         algorithm.options['max_population_size'] = 100
+#         algorithm.run()
+#
+#         b = Results(problem)
+#         optimum = b.find_minimum('F_1')  # Takes last cost function
+#         self.assertAlmostEqual(optimum.costs[0], method.bias, 0)
+#
+#     # unimodal problems
+#
+#     def test_shifted_sphere(self):
+#         self.run_test_problem(optp.cec2005.F1, 100, -100, 100)
+#
+#     def test_shifted_double_sum(self):
+#         self.run_test_problem(optp.cec2005.F2, 100, -100, 100)
+#
+#     # def test_shifted_rotated_elliptic(self): # wrong with platypus also
+#     #     self.run_test_problem(optp.cec2005.F3, 100, -100, 10000)
+#     #
+#     #def test_shifted_schwefel(self): # wrong with platypus also
+#     #    self.run_test_problem(optp.cec2005.F4, 100, -100, 10000)
+#
+#     def test_schwefel(self): # !
+#         self.run_test_problem(optp.cec2005.F5, 100, -100, 100)
+#
+#     def test_shifted_rosenbrock(self):
+#         self.run_test_problem(optp.cec2005.F6, 100, -100, 1000)
+#
+#     # multimodal problems
+#     #def test_shifted_rot_griewank(self):
+#     #    self.run_test_problem(optp.cec2005.F7, 100, -100, 10000)
+#
+#     #def test_shifted_rot_ackley(self):
+#     #    self.run_test_problem(optp.cec2005.F8, 32, -32, 100)
+#
+#     def test_shifted_rastrigin(self):
+#         self.run_test_problem(optp.cec2005.F9, 5, -5, 100)
+#
+#     def test_shifted_rot_rastrigin(self):
+#         self.run_test_problem(optp.cec2005.F10, 5, -5, 1000)
+#
+#     def test_shifted_rot_weierstrass(self):
+#         self.run_test_problem(optp.cec2005.F11, 0.5, -0.5, 100)
+#
+#     def test_fletcherpowell(self): #schwefel
+#         self.run_test_problem(optp.cec2005.F12, pi, -pi, 100)
+#
+#     def test_f8f2(self):
+#         self.run_test_problem(optp.cec2005.F13, 1, -3, 100)
+#
+#     def test_expanded_f6(self):
+#         self.run_test_problem(optp.cec2005.F14, 100, -100, 100)
+#
+#     #####
+#     def test_hybrid_composition(self):
+#         self.run_test_problem(optp.cec2005.F15, 5, -5, 100)
+#
+#     def test_rotated_f15(self):
+#         self.run_test_problem(optp.cec2005.F16, 5, -5, 100)
+#
+#     def test_f16_with_noise(self):
+#         self.run_test_problem(optp.cec2005.F17, 5, -5, 250)
+#
+#     # def test_rot_hybrid_function(self):
+#     #     self.run_test_problem(optp.cec2005.F18, 5, -5, 1000)
+#     #
+#     # def test_rot_hybrid_function_2(self):
+#     #     self.run_test_problem(optp.cec2005.F19, 5, -5, 1000)
+#     #
+#     # def test_rot_hybrid_function_3(self):
+#     #     self.run_test_problem(optp.cec2005.F20, 5, -5, 1000)
+#     #
+#     # def test_rot_hybrid_function_4(self):
+#     #     self.run_test_problem(optp.cec2005.F21, 5, -5, 1000)
+#     #
+#     # def test_rot_hybrid_function_5(self):
+#     #     self.run_test_problem(optp.cec2005.F22, 5, -5, 1000)
+#     #
+#     # def test_rot_hybrid_function_6(self):
+#     #     self.run_test_problem(optp.cec2005.F23, 5, -5, 1000)
+#     #
+#     # def test_rot_hybrid_function_7(self):
+#     #     self.run_test_problem(optp.cec2005.F24, 5, -5, 1000)
+#     #
+#     # def test_rot_hybrid_function_wo_bounds(self):
+#     #     self.run_test_problem(optp.cec2005.F25, 100, -100, 100)
+#
 
 #####
 #####
