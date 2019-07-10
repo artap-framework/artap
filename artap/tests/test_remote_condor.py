028faf06f459a32ef22e32724d7cac71ddeefe2c
David Panek <panek50@kte.zcu.cz>
2019-07-10 10:25:00 +0200

    work on executors
    Former-commit-id: e9950bd1cdfdfb593e4997ef4f9c46c457433dcb

--- artap/tests/test_remote_condor.py
+++ artap/tests/test_remote_condor.py
@@ -1,34 +1,32 @@
-import os
 from unittest import TestCase, main
 
-from artap.executor import CondorComsolJobExecutor
+from artap.executor import CondorComsolJobExecutor, CondorMatlabJobExecutor, CondorPythonJobExecutor
 from artap.problem import Problem, ProblemType
 from artap.population import Population
 from artap.algorithm import DummyAlgorithm
 
 
-# class CondorMatlabProblem(Problem):
-#     """ Describe simple one objective optimization problem. """
-#
-#     def set(self):
-#         self.name = "CondorMatlabProblem"
-#         self.parameters = [{'name': 'a', 'initial_value': 10, 'bounds': [0, 20]},
-#                       {'name': 'b', 'initial_value': 10, 'bounds': [5, 15]}]
-#         self.costs = [{'name': 'F_1'}]
-#         self.working_dir = "." + os.sep + "data" + os.sep
-#         self.type = ProblemType.matlab
-#         self.executor = CondorJobExecutor(self,
-#                                           command="run.sh",
-#                                           model_file="run_input.m",
-#                                           input_file="input.txt",
-#                                           output_file="output.txt",
-#                                           supplementary_files=["run.sh"])
-#
-#     def evaluate(self, individual):
-#         return self.executor.eval(individual.vector)    # ToDo: could be passed individual?
-#
-#     def parse_results(self, content):
-#         return [float(content)]
+class CondorMatlabProblem(Problem):
+    """ Describe simple one objective optimization problem. """
+
+    def set(self):
+        self.name = "CondorMatlabProblem"
+        self.parameters = [{'name': 'a', 'initial_value': 10, 'bounds': [0, 20]},
+                      {'name': 'b', 'initial_value': 10, 'bounds': [5, 15]}]
+        self.costs = [{'name': 'F_1'}]
+        self.type = ProblemType.matlab
+        self.executor = CondorMatlabJobExecutor(self,
+                                                script="./data/run_input.m",
+                                                parameter_file="input.txt",
+                                                files_from_condor=["output.txt"])
+
+    def evaluate(self, individual):
+        return self.executor.eval(individual)    # ToDo: could be passed individual?
+
+    def parse_results(self, output_files, individual):
+        with open(output_files[0]) as file:
+            content = file.readlines()
+        return [float(content[0])]
 
 
 class CondorComsolProblem(Problem):
@@ -41,12 +39,13 @@ class CondorComsolProblem(Problem):
 
         self.costs = [{'name': 'F_1'}]
         self.type = ProblemType.comsol
-        self.executor = CondorComsolJobExecutor(self, model_file="./data/elstat.mph", output_files=["out.txt", "elstat.mph"],)
+        self.executor = CondorComsolJobExecutor(self, model_file="./data/elstat.mph",
+                                                files_from_condor=["out.txt", "elstat.mph"], )
 
     def evaluate(self, individual):
-        return self.executor.eval(individual.vector)
+        return self.executor.eval(individual)
 
-    def parse_results(self, output_files):
+    def parse_results(self, output_files, individual):
         with open(output_files[0]) as file:
             content = file.readlines()
         line_with_results = content[5]  # 5th line contains results
@@ -54,62 +53,63 @@ class CondorComsolProblem(Problem):
         return [result]
 
 
-# class PythonExecProblem(Problem):
-#     """ Describe simple one objective optimization problem. """
-#     def set(self):
-#         self.parameters = [{'name': 'a', 'initial_value': 10, 'bounds': [0, 20]},
-#                            {'name': 'b', 'initial_value': 10, 'bounds': [5, 15]}]
-#         self.costs = [{'name': 'F_1'}]
-#         self.working_dir = "." + os.sep + "data" + os.sep
-#         self.type = ProblemType.python
-#         self.executor = CondorJobExecutor(self,
-#                                           command="/usr/bin/python",
-#                                           model_file="run_exec.py",
-#                                           output_file="output.txt")
-#
-#     def evaluate(self, individual):
-#         return self.executor.eval(individual.vector)
-#
-#     def parse_results(self, content):
-#         return [float(content)]
-#
-#
-# class PythonInputProblem(Problem):
-#     """ Describe simple one objective optimization problem. """
-#     def set(self):
-#         self.parameters = [{'name': 'a', 'initial_value': 10, 'bounds': [0, 20]},
-#                            {'name': 'b', 'initial_value': 10, 'bounds': [5, 15]}]
-#         self.costs = [{'name': 'F_1'}]
-#         self.working_dir = "." + os.sep + "data" + os.sep
-#         self.type = ProblemType.python
-#         self.executor = CondorJobExecutor(self,
-#                                           command="/usr/bin/python",
-#                                           model_file="run_input.py",
-#                                           input_file="input.txt",  # file is created in eval with specific parameters
-#                                           output_file="output.txt")
-#
-#     def evaluate(self, individual):
-#         return self.executor.eval(individual.vector)
-#
-#     def parse_results(self, content):
-#         return [float(content)]
+class PythonExecProblem(Problem):
+    """ Describe simple one objective optimization problem. """
+    def set(self):
+        self.parameters = [{'name': 'a', 'initial_value': 10, 'bounds': [0, 20]},
+                           {'name': 'b', 'initial_value': 10, 'bounds': [5, 15]}]
+        self.costs = [{'name': 'F_1'}]
+        self.type = ProblemType.python
+        self.executor = CondorPythonJobExecutor(self,
+                                                script="./data/run_exec.py",
+                                                parameter_file=None,
+                                                output_files=["output.txt"])
+
+    def evaluate(self, individual):
+        return self.executor.eval(individual)
+
+    def parse_results(self, output_files, individual):
+        with open(output_files[0]) as file:
+            content = file.readlines()
+        return [float(content[0])]
+
+
+class PythonInputProblem(Problem):
+    """ Describe simple one objective optimization problem. """
+    def set(self):
+        self.parameters = [{'name': 'a', 'initial_value': 10, 'bounds': [0, 20]},
+                           {'name': 'b', 'initial_value': 10, 'bounds': [5, 15]}]
+        self.costs = [{'name': 'F_1'}]
+        self.type = ProblemType.python
+        self.executor = CondorPythonJobExecutor(self,
+                                                script="./data/run_input.py",
+                                                parameter_file="input.txt",
+                                                output_files=["output.txt"])
+
+    def evaluate(self, individual):
+        return self.executor.eval(individual)
+
+    def parse_results(self, output_files, individual):
+        with open(output_files[0]) as file:
+            content = file.readlines()
+        return [float(content[0])]
 
 
 class TestCondor(TestCase):
     """ Tests simple optimization problem where calculation of
         goal function is submitted as a job on HtCondor.
     """
-    # def test_condor_matlab_input(self):
-    #     """ Tests one calculation of goal function."""
-    #     problem = CondorMatlabProblem()
-    #
-    #     table = [[1, 2]]
-    #     population = Population()
-    #     population.gen_population_from_table(table)
-    #     evaluator = DummyAlgorithm(problem)
-    #     evaluator.evaluate(population.individuals)
+    def test_condor_matlab_input(self):
+        """ Tests one calculation of goal function."""
+        problem = CondorMatlabProblem()
 
-    #    self.assertAlmostEqual(5, population.individuals[0].costs[0])
+        table = [[1, 2]]
+        population = Population()
+        population.gen_population_from_table(table)
+        evaluator = DummyAlgorithm(problem)
+        evaluator.evaluate(population.individuals)
+
+        self.assertAlmostEqual(5, population.individuals[0].costs[0])
 
     def test_condor_comsol_exec(self):
         """ Tests one calculation of goal function."""
@@ -125,27 +125,27 @@ class TestCondor(TestCase):
         self.assertAlmostEqual(112.94090668383139, population.individuals[0].costs[0])
         self.assertAlmostEqual(124.23499735221547, population.individuals[1].costs[0])
 
-    # def test_condor_python_exec(self):
-    #     problem = PythonExecProblem()
-    #
-    #     table = [[1, 2]]
-    #     population = Population()
-    #     population.gen_population_from_table(table)
-    #     evaluator = DummyAlgorithm(problem)
-    #     evaluator.evaluate(population.individuals)
-    #
-    #     self.assertAlmostEqual(5, population.individuals[0].costs[0])
-    #
-    # def test_condor_python_input(self):
-    #     problem = PythonInputProblem()
-    #
-    #     table = [[1, 2]]
-    #     population = Population()
-    #     population.gen_population_from_table(table)
-    #     evaluator = DummyAlgorithm(problem)
-    #     evaluator.evaluate(population.individuals)
-    #
-    #     self.assertAlmostEqual(5, population.individuals[0].costs[0])
+    def test_condor_python_exec(self):
+        problem = PythonExecProblem()
+
+        table = [[1, 2]]
+        population = Population()
+        population.gen_population_from_table(table)
+        evaluator = DummyAlgorithm(problem)
+        evaluator.evaluate(population.individuals)
+
+        self.assertAlmostEqual(5, population.individuals[0].costs[0])
+
+    def test_condor_python_input(self):
+        problem = PythonInputProblem()
+
+        table = [[1, 2]]
+        population = Population()
+        population.gen_population_from_table(table)
+        evaluator = DummyAlgorithm(problem)
+        evaluator.evaluate(population.individuals)
+
+        self.assertAlmostEqual(5, population.individuals[0].costs[0])
 
 
 if __name__ == '__main__':
