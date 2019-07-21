bcb8a76f088e00a4b18bf7d213d0aa2482c9fe84
Pavel Karban <pkarban@gmail.com>
2019-07-21 10:54:20 +0200

    remove remote dir, fixes
    Former-commit-id: f92cd5496d91a157726bf15595de6af5f933c91c

--- artap/executor.py
+++ artap/executor.py
@@ -530,8 +530,10 @@ class CondorJobExecutor(RemoteExecutor):
                 else:
                     assert 0
 
+                self._remove_remote_dir(remote_dir=remote_dir, client=client)
+
                 client.close()
-                
+
                 return result
 
             except ConnectionError as e:
