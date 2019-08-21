9f61e14d7b25f81f038bf55f10f20d26ea4429d4
Pavel Karban <pkarban@gmail.com>
2019-08-21 14:55:57 +0200

    optional datastore - sqlitedict
    Former-commit-id: 37437bcb21e8a23a8af46b4f10d75214e674ec9d [formerly e79f2bbe58a2e0331366b3f0f0643bbdfc1fbb8b [formerly e9ccabc8d07097cb19dd5b126a8a8128c08e43ca]]
    Former-commit-id: e392d3dae5da07037f5c35f88d0c8672627908b6
    Former-commit-id: 46214c146bbb7f8fdca062cf846c134d0a396c32

--- artap/tests/test_datastore.py
+++ artap/tests/test_datastore.py
@@ -25,7 +25,7 @@ class MyProblem(Problem):
 
 class TestDataStoreFile(unittest.TestCase):
 
-    def test_read_data_store(self):
+    def test_read_dbm_data_store(self):
         problem = Problem()
         results = Results(problem)
         database_name = "." + os.sep + "data" + os.sep + "data.db"
@@ -45,7 +45,7 @@ class TestDataStoreFile(unittest.TestCase):
         self.assertAlmostEqual(individual.vector[1], 1.83185007711266, 5)
         self.assertAlmostEqual(individual.costs[0], 5.378264283347013, 5)
 
-    def test_write_data_store(self):
+    def test_write_dbm_data_store(self):
         problem = MyProblem()
 
         # set data store
@@ -67,6 +67,41 @@ class TestDataStoreFile(unittest.TestCase):
         import shelve
 
         db = shelve.open(problem.data_store.database_name, flag='r')
+        populations = db["populations"]
+
+        self.assertAlmostEqual(populations[-1].individuals[9].costs[0], 1.854, 3) # result
+        db.close()
+
+        # remove file
+        # print(database_name)
+        os.remove(database_name)
+
+    def test_write_sqlitedict_data_store(self):
+        problem = MyProblem()
+
+        # set data store
+        database_name = tempfile.NamedTemporaryFile(mode="w", delete=False, dir=None, suffix=".sqlite").name
+        problem.data_store = FileDataStore(problem, database_name=database_name, backend="sqlitedict", mode="write")
+        algorithm = NLopt(problem)
+        algorithm.options['verbose_level'] = 0
+        algorithm.options['algorithm'] = LN_BOBYQA
+        algorithm.options['n_iterations'] = 10
+        algorithm.run()
+
+        results = Results(problem)
+        optimum = results.find_minimum('F')
+        self.assertAlmostEqual(optimum.costs[0], 1.854, 3)
+
+        problem.data_store.db.close()
+
+        # check db
+        from artap.environment import Enviroment
+        import sys
+        sys.path.append(Enviroment.artap_root + os.sep + "../3rdparty" + os.sep + "sqlitedict")
+        from sqlitedict import SqliteDict
+
+        db = SqliteDict(database_name, autocommit=True)
+
         populations = db["populations"]
 
         self.assertAlmostEqual(populations[-1].individuals[9].costs[0], 1.854, 3) # result
