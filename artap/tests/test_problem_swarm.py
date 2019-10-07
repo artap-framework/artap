9e8045a628e3f59b74d6d22fcc2dd0980fe99d85
Tamás <orosz.tamas@yahoo.com>
2019-10-07 22:06:08 +0200

    -> standardized tests for swarm optimization
    Former-commit-id: a8958bdc7416347431fb19102e7ef8b854117367

--- artap/tests/test_problem_swarm.py
+++ artap/tests/test_problem_swarm.py
@@ -1,48 +1,91 @@
 import unittest
 from artap.problem import Problem
 from artap.algorithm_swarm import PSO
-from artap.benchmark_functions import BinhAndKorn
+from artap.results import Results
+
+import optproblems as optp
+import optproblems.cec2005 as cec2005
 
 
-class PSORosenbrock(Problem):
-    """ Search the optimal value of the Rosenbrock function in 2d"""
+class CEC2005_TEST_Problems(Problem):
+    """
+    Template class to initialize the different test problems these problems working for 2, 10, 30 variables.
+
+    There are 25 test problems, which were compiled for the Special Session on Real-Parameter Optimization at the
+    Congress on Evolutionary Computation (CEC), Edinburgh, UK, 2-5 Sept. 2005. The mathematical definitions are given in
+    [Suganthan2005]. Additionally, implementations in C, Java, and Matlab were provided for the participants.
+    Artap uses the optproblems test library to run these test. This template class is made to invoke them.
+    """
 
     def set(self):
-        self.name = "PSORosenbrock"
-        self.parameters = [{'name': 'x_1', 'initial_value': 2.5, 'bounds': [0, 5]},
-                      {'name': 'x_2', 'initial_value': 1.5, 'bounds': [0, 3]}]
-        self.costs = [{'name': 'F_1'},
-                      {'name': 'F_2'}]
+        # two variables
+        self.parameters = [{'name': 'x_1', 'bounds': [func.min_bounds[0], func.max_bounds[0]]},
+                           {'name': 'x_2', 'bounds': [func.min_bounds[1], func.max_bounds[1]]}]
 
-    def evaluate(self, individual):
-        function = BinhAndKorn()
-        return function.eval(individual.vector)
+        self.costs = [{'name':'F_1'}]
 
 
-class TestPSOptimization(unittest.TestCase):
-    """ Tests simple one objective optimization problem."""
+    def evaluate(self, x):
+        problem = optp.Problem(func, num_objectives=1)
+        solutions = [optp.Individual(x.vector)]
 
-    def test_local_problem_pso(self):
-        problem = PSORosenbrock()
+        problem.batch_evaluate(solutions)
+
+        return [solutions[0].objective_values]
+
+
+class TestCEC2005(unittest.TestCase):
+    """Tests from CEC2005 library"""
+
+    def run_test_problem(self, nr_gen, function):
+        global func
+        func = function
+        problem = CEC2005_TEST_Problems()
         algorithm = PSO(problem)
-        algorithm.options['max_population_number'] = 10
+        algorithm.options['max_population_number'] = nr_gen
         algorithm.options['max_population_size'] = 100
         algorithm.run()
 
-        # b = Results(problem)
-        # solution = b.pareto_values()
-        # wrong = 0
-        # for sol in solution:
-        #     if abs(BinhAndKorn.approx(sol[0]) - sol[1]) > 0.1 * BinhAndKorn.approx(sol[0]) \
-        #             and 20 < sol[0] < 70:
-        #         wrong += 1
-        #
-        # self.assertLessEqual(wrong, 5)
+        b = Results(problem)
+        optimum = b.find_minimum('F_1')  # Takes last cost function
+        self.assertAlmostEqual(optimum.costs[0], func.bias, 0)
+        del func
 
-        # results = GraphicalResults(problem)
-        # results.plot_scatter_vectors('x_1', 'x_2', filename="/tmp/scatter_vec.pdf", population_number=10)
-        # results.plot_scatter('F_1', 'F_2', filename="/tmp/scatter.pdf", population_number=None)
+    def test_shifted_sphere(self):
+        self.run_test_problem(300, optp.cec2005.F1(2))
 
+    def test_shifted_double_sum(self):
+        self.run_test_problem(300, optp.cec2005.F2(2))
+
+    def test_schwefel(self):  # !
+        self.run_test_problem(400, optp.cec2005.F5(2))
+
+    def test_shifted_rosenbrock(self):
+        self.run_test_problem(300, optp.cec2005.F6(2))
+
+    def test_shifted_rastrigin(self):
+        self.run_test_problem(300, optp.cec2005.F9(2))
+
+    def test_shifted_rot_weierstrass(self):
+        self.run_test_problem(300, optp.cec2005.F11(2))
+
+    def test_fletcherpowell(self):  # schwefel
+        self.run_test_problem(300, optp.cec2005.F12(2))
+
+    def test_f8f2(self):
+        self.run_test_problem(300, optp.cec2005.F13(2))
+
+    def test_expanded_f6(self):
+        self.run_test_problem(300, optp.cec2005.F14(2))
+
+    def test_hybrid_composition(self):
+        self.run_test_problem(300, optp.cec2005.F15(2))
+
+    def test_rotated_f15(self):
+        self.run_test_problem(300, optp.cec2005.F16(2))
+
+    def test_f17_with_noise(self):
+        self.run_test_problem(1000, optp.cec2005.F17(2))
 
 if __name__ == '__main__':
     unittest.main()
