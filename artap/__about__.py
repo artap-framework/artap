078796dfbeceded592857d6fb766d7f9a0276f72
Pavel Karban <pkarban@gmail.com>
2019-10-22 19:43:38 +0200

    ssh support removed
    Former-commit-id: 659c8850c5054536c779f7a70dd69b76cf661f36

--- artap/__about__.py
+++ artap/__about__.py
@@ -2,6 +2,6 @@ __version__ = "0.1.6"
 __author__ = u"Artap Team"
 __author_email__ = "artap.team@gmail.com"
 __copyright__ = u"Copyright (c) 2018-2019, {} <{}>".format(__author__, __author_email__)
-__website__ = "https://edison.fel.zcu.cz:444/agros/RDOLab/Artap"
+__website__ = "http://agros2d.org/artap"
 __license__ = "License :: OSI Approved :: MIT License"
 __status__ = "Development Status :: 3 - Alpha"
