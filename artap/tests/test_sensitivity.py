ba96ef86e6b8be19a911f6d6065adbb1d13012cf
Pavel Karban <pkarban@gmail.com>
2019-11-02 22:03:28 +0100

    sensitivity analysis library introduced - SALib
    Former-commit-id: d679ec84c7e9d762605eed338e0b5bdc67fdee01

--- artap/tests/test_sensitivity.py
+++ artap/tests/test_sensitivity.py
@@ -1,7 +1,44 @@
 import unittest
 
 from artap.problem import Problem
-from artap.algorithm_sensitivity import Sensitivity
+from artap.algorithm_sensitivity import Sensitivity, SALibAlgorithm
+
+import math
+
+
+class SensitivitySALibProblem(Problem):
+    def set(self):
+        self.name = "SensitivityTestProblem"
+        self.parameters = [{'name': 'A', 'initial_value': 0.0, 'bounds': [-3.14159, 3.14159]},
+                           {'name': 'B', 'initial_value': 0.0, 'bounds': [-3.14159, 3.14159]},
+                           {'name': 'C', 'initial_value': 0.0, 'bounds': [-3.14159, 3.14159]}]
+
+        self.costs = [{'name': 'F_1'}]
+        # self.executor = CondorComsolJobExecutor(self, model_file="./data/coil_linear_current.mph",
+        #                                        files_from_condor=["temp.txt"])
+
+    def evaluate(self, individual):
+        return math.sin(individual.vector[0]) + 7.0 * math.pow(math.sin(individual.vector[1]), 2) + \
+               0.1 * math.pow(individual.vector[2], 4) * math.sin(individual.vector[0])
+
+
+class TestSALibSensitivity(unittest.TestCase):
+    """ Tests simple one objective optimization problem."""
+
+    def test_sobol_problem(self):
+        problem = SensitivitySALibProblem()
+        problem.options['save_data_files'] = True
+
+        algorithm = SALibAlgorithm(problem)
+        algorithm.options["samples"] = 1000
+        algorithm.options["method"] = "sobol"
+        algorithm.options["print_to_console"] = False
+        algorithm.run()
+
+        res = algorithm.analyze()
+        self.assertAlmostEqual(res["S1"][0], 0.30797525)
+        self.assertAlmostEqual(res["S1"][1], 0.44776754)
+        self.assertAlmostEqual(res["S1"][2], -0.00425456)
 
 
 class MyProblem(Problem):
