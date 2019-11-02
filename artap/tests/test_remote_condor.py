24dce19c857466fff50554df6bca55b6b65a357d
Pavel Karban <pkarban@gmail.com>
2019-11-02 14:41:26 +0100

    faster remote condor test
    Former-commit-id: 892c963210f5445942e95950d57dc79ae0debc1b

--- artap/tests/test_remote_condor.py
+++ artap/tests/test_remote_condor.py
@@ -142,13 +142,13 @@ class TestCondor(TestCase):
     def test_condor_python_exec_full_load(self):
         problem = PythonExecProblem()
 
-        n = 500
+        n = 200
         individuals = []
         for i in range(n):
             individuals.append(Individual([random.randrange(1, 100), random.randrange(1, 100)]))
 
         algorithm = DummyAlgorithm(problem)
-        algorithm.options['max_processes'] = 300
+        algorithm.options['max_processes'] = 70
         algorithm.evaluate(individuals)
 
         for i in range(n):
