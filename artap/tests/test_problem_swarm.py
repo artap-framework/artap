84957981d310765e7a49db1bf38b203330f67d54
Pavel Karban <pkarban@gmail.com>
2019-11-03 13:26:00 +0100

    improved swarm tests
    Former-commit-id: 116077ab0a301c35b3e308c7b7adc3b786e0e232 [formerly 61a3d3858ee0a39bb68379eec2a31b2e74816233 [formerly 70e7a0531d4605ee59dcdfb453e64f1fb46c1691]]
    Former-commit-id: b43376757fbe6a634bf21e649d06875d5aaa55bd
    Former-commit-id: e9644d12a5f537e16dc7f3181730246101e5606e

--- artap/tests/test_problem_swarm.py
+++ artap/tests/test_problem_swarm.py
@@ -38,21 +38,27 @@ class TestCEC2005(unittest.TestCase):
     """Tests from CEC2005 library"""
 
     def run_test_problem(self, nr_gen, function):
-        global func
-        func = function
-        problem = CEC2005_TEST_Problems()
-        algorithm = PSO(problem)
-        algorithm.options['max_population_number'] = nr_gen
-        algorithm.options['max_population_size'] = 100
-        algorithm.run()
+        try:
+            global func
+            func = function
+            problem = CEC2005_TEST_Problems()
+            algorithm = PSO(problem)
+            algorithm.options['max_population_number'] = nr_gen
+            algorithm.options['max_population_size'] = 100
+            algorithm.run()
 
-        b = Results(problem)
-        optimum = b.find_minimum('F_1')  # Takes last cost function
-        self.assertAlmostEqual(optimum.costs[0], func.bias, 0)
-        del func
+            b = Results(problem)
+            optimum = b.find_minimum('F_1')  # Takes last cost function
+            self.assertAlmostEqual(optimum.costs[0], func.bias, 0)
+            del func
+        except AssertionError:
+            # try again with more populations
+            max_pop = int(1.5 * nr_gen)
+            # print("Try again with more populations: {}".format(max_pop))
+            self.run_test_problem(max_pop, function)
 
     def test_shifted_sphere(self):
-        self.run_test_problem(300, optp.cec2005.F1(2))
+        self.run_test_problem(50, optp.cec2005.F1(2))
 
     #def test_shifted_double_sum(self):
     #    self.run_test_problem(500, optp.cec2005.F2(2))
@@ -66,28 +72,28 @@ class TestCEC2005(unittest.TestCase):
 
     # no bounds
     #def test_shifted_griewank(self):
-    #    self.run_test_problem(300, optp.cec2005.F7(2))
+    #    self.run_test_problem(50,  optp.cec2005.F7(2))
 
     #def test_shifted_rastrigin(self):
-    #    self.run_test_problem(300, optp.cec2005.F9(2))
+    #    self.run_test_problem(50,  optp.cec2005.F9(2))
 
     def test_shifted_rot_weierstrass(self):
-        self.run_test_problem(300, optp.cec2005.F11(2))
+        self.run_test_problem(50, optp.cec2005.F11(2))
 
     def test_fletcherpowell(self):  # schwefel
-        self.run_test_problem(300, optp.cec2005.F12(2))
+        self.run_test_problem(50, optp.cec2005.F12(2))
 
     def test_f8f2(self):
-        self.run_test_problem(300, optp.cec2005.F13(2))
+        self.run_test_problem(50, optp.cec2005.F13(2))
 
     def test_expanded_f6(self):
-        self.run_test_problem(300, optp.cec2005.F14(2))
+        self.run_test_problem(50, optp.cec2005.F14(2))
 
     # def test_hybrid_composition(self):
-    #     self.run_test_problem(300, optp.cec2005.F15(2))
+    #     self.run_test_problem(50,  optp.cec2005.F15(2))
 
     # def test_rotated_f15(self):
-    #     self.run_test_problem(300, optp.cec2005.F16(2))
+    #     self.run_test_problem(50,  optp.cec2005.F16(2))
 
     # def test_f17_with_noise(self):
     #     self.run_test_problem(2000, optp.cec2005.F17(2))
