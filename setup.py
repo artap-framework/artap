783b98303e155c0ffc4f0acdde69a53ab5df8f07
David Pánek <panek50@kte.zcu.cz>
2020-02-06 13:14:53 +0100

    added custom command to setup.py
    Former-commit-id: 229d73e2d17ec3389c3128d309d8cdb98d5ffa8d

--- setup.py
+++ setup.py
@@ -1,6 +1,17 @@
 import setuptools
-from setuptools.command.build_ext import build_ext as _build_ext
+from setuptools.command.install import install
+import subprocess
 import os
+
+
+class CustomInstallCommand(install):
+    """Custom install setup to help run shell commands (outside shell) before installation"""
+
+    def run(self):
+        os.system('pip3 install numpy')
+        os.system('pip3 install cython')
+        install.run(self)
+
 
 base_dir = os.path.abspath(os.path.dirname(__file__))
 
@@ -14,7 +25,7 @@ with open("requirements.txt", "r") as fh:
 
 setuptools.setup(
     name="artap",
-    version="2020.2.4",
+    version="2020.2.6",
     author=u"Artap Team",
     author_email="artap.framework@gmail.com",
     description="Platform for robust design optimization",
@@ -23,6 +34,7 @@ setuptools.setup(
     url="http://www.agros2d.org/artap/",
     python_requires='>3.6',
     license="License :: OSI Approved :: MIT License",
+    cmdclass={'install': CustomInstallCommand},
     packages=setuptools.find_packages(),
     include_package_data=True,
     data_files=[('artap/lib', ['artap/lib/bayesopt.so']),
