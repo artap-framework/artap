45c7b1518b6087d7428d5d421b2f23ea49bebe6c
Pavel Karban <karban@kte.zcu.cz>
2020-02-09 19:55:13 +0100

    agros removed - prefered way is to install pip package agrossuite (due to problems with datadir must be in ~/.local - symlink is ok)
    Former-commit-id: 7d0e7c67211272b24f7d13feadc4aeb198205dd9 [formerly d3faea5efe5bce6bd44e6944254331ccffc25a3a [formerly 30d897d82fd9796c8d73837e3132fc08294e71f3]]
    Former-commit-id: 8f4727065b75f73c28bc77d7736e22ed7af125c5
    Former-commit-id: 83ff2bc5efa8c33096a53495ef887bcdc53bf49d

--- 3rdparty/submodules.sh
+++ 3rdparty/submodules.sh
@@ -4,7 +4,6 @@ DIR=$PWD
 
 # git submodule add git@edison.fel.zcu.cz:agros/RDOLab/3rdparty/nlopt2.git 3rdparty/nlopt2
 # git submodule add git@edison.fel.zcu.cz:agros/RDOLab/3rdparty/bayesopt.git 3rdparty/bayesopt
-# git submodule add git@edison.fel.zcu.cz:agros/agros2d.git 3rdparty/agros
 
 # update
 git submodule update --init --recursive
@@ -27,34 +26,3 @@ cmake ..
 make -j2
 cp $DIR/bayesopt/build/lib/bayesopt.so $DIR/../artap/lib/
 strip $DIR/../artap/lib/bayesopt.so
-
-# Agros Suite
-cd $DIR/agros
-git submodule update --init 
-cd dealii 
-mkdir -p build 
-cd build 
-cmake -DCMAKE_BUILD_TYPE="Release" -DDEAL_II_WITH_CXX14="OFF" -DDEAL_II_WITH_CXX17="OFF" ..
-make -j2 
-cd ../../
-# - export CC=/usr/bin/clang
-# - export CXX=/usr/bin/clang++     
-cmake . 
-make -j2
-# generator
-./agros_generator
-cd plugins
-cmake .
-make -j2
-# strip
-cd ..
-strip libs/*.so
-strip agros
-strip agros_solver
-strip pythonlab
-# link ~/.local
-ln -sfn $DIR/agros ~/.local/lib/python3.7/site-packages/agrossuite
-#ln -sfn $DIR/agros ~/.local/lib/python3.6/site-packages/agrossuite
-#ln -sfn $DIR/agros /home/tamas/anaconda3/envs/artapenc/lib/python3.6/site-packages/agrossuite
-# go back
-cd $DIR
