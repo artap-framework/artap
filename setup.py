9e8b9f470b8dad51d089c82051e08263061e41ed
Pavel Karban <karban@kte.zcu.cz>
2020-02-09 21:06:26 +0100

    fixed agros test on c4
    Former-commit-id: 6dd34f9cd64797223e59b3e96da92c84650fd69c

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ with open("requirements.txt", "r") as fh:
 
 setuptools.setup(
     name="artap",
-    version="2020.2.9",
+    version="2020.2.9.1",
     author=u"Artap Team",
     author_email="artap.framework@gmail.com",
     description="Platform for robust design optimization",
