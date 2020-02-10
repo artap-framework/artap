5306e293c27e9dee198862324accbdfbfa744291
Pavel Karban <karban@kte.zcu.cz>
2020-02-10 14:24:54 +0100

    nlopt removed - pypi replace
    Former-commit-id: cd67f9d4dfb0b8270479fd9ebd2add0ab853b3fe

--- 3rdparty/submodules.sh
+++ 3rdparty/submodules.sh
@@ -2,27 +2,16 @@
 
 DIR=$PWD
 
-# git submodule add git@edison.fel.zcu.cz:agros/RDOLab/3rdparty/nlopt2.git 3rdparty/nlopt2
 # git submodule add git@edison.fel.zcu.cz:agros/RDOLab/3rdparty/bayesopt.git 3rdparty/bayesopt
 
 # update
 git submodule update --init --recursive
-
-# nlopt2
-cd $DIR/nlopt2
-mkdir -p build
-cd build
-cmake ..
-make -j2
-cp $DIR/nlopt2/build/src/swig/_nlopt.so $DIR/../artap/lib/_nlopt.so
-cp $DIR/nlopt2/build/src/swig/__nlopt.so $DIR/../artap/lib/_nlopt.so
-strip $DIR/../artap/lib/_nlopt.so
 
 # BayesOpt
 cd $DIR/bayesopt
 mkdir -p build
 cd build
 cmake ..
-make -j2
+make -j5
 cp $DIR/bayesopt/build/lib/bayesopt.so $DIR/../artap/lib/
 strip $DIR/../artap/lib/bayesopt.so
