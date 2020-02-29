af010bbda01bb84a9a897975bbfb056313bb04bd
Tamás <orosz.tamas@yahoo.com>
2020-03-01 00:01:10 +0100

    n dimensional schwefel function added
    Former-commit-id: 626efe86f9ea5535645c3ba4756db109085d3f07

--- artap/tests/test_benchmarks.py
+++ artap/tests/test_benchmarks.py
@@ -1,27 +1,45 @@
 import unittest
-from artap.benchmark_functions import Rosenbrock, Ackley
+from artap.benchmark_functions import Rosenbrock, Ackley, Schwefel, Sphere
 
 
 class TestRosenbrock(unittest.TestCase):
 
     def test_rosenbrock2d(self):
-        test2d = Rosenbrock(**{'dimension':2})
+        test2d = Rosenbrock(**{'dimension': 2})
         self.assertAlmostEqual(test2d.evaluate([1.0, 1.0]), 0.0)
 
     def test_rosenbrock10d(self):
-        test2d = Rosenbrock(**{'dimension':10})
+        test2d = Rosenbrock(**{'dimension': 10})
         self.assertAlmostEqual(test2d.evaluate([1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]), 0.0)
+
 
 class TestAckley(unittest.TestCase):
     """ Tests for the synthetic benchmark functions """
 
     def test_ackley(self):
-        test2d = Ackley(**{'dimension':2})
+        test2d = Ackley(**{'dimension': 2})
         self.assertAlmostEqual(test2d.evaluate([0.0, 0.0]), 0.0)
 
     def test_ackley10d(self):
-        test2d = Ackley(**{'dimension':10})
+        test2d = Ackley(**{'dimension': 10})
         self.assertAlmostEqual(test2d.evaluate([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]), 0.0)
+
+
+class TestSphere(unittest.TestCase):
+
+    def test_sphere(self):
+        test7d = Sphere(**{'dimension': 7})
+        self.assertAlmostEqual(test7d.evaluate(test7d.global_optimum_coords), 0.0)
+
+
+class TestSchwefel(unittest.TestCase):
+
+    def test_schwefel(self):
+        test2d = Schwefel(**{'dimension': 2})
+        self.assertAlmostEqual(test2d.evaluate([420.9687, 420.9687]), 0, 4)
+
+        test10d = Schwefel(**{'dimension': 10})
+        self.assertAlmostEqual(test2d.evaluate(test2d.global_optimum_coords), 0, 4)
 
 
 if __name__ == '__main__':
