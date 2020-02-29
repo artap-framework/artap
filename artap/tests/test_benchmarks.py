94011a5eadd639b63b34f36459fee3b852e08b70
Tamás <orosz.tamas@yahoo.com>
2020-02-29 01:00:17 +0100

    n dimensional ackley function
    Former-commit-id: c1bbf1516a328fa790927760c895848640f7ea3e [formerly 3ccaa36dae57617f7e9aa1249c4bb2e3eef483a7 [formerly 271df592472feb9a8f48ebbc713b85b945d77c0d]]
    Former-commit-id: 0879d6f71a406899af702b2ce94c1df065b29a9d
    Former-commit-id: 73ab421bfffca9ac84fa7c191f9d45b18c461760

--- artap/tests/test_benchmarks.py
+++ artap/tests/test_benchmarks.py
@@ -1,9 +1,8 @@
 import unittest
-from artap.benchmark_functions import Rosenbrock
+from artap.benchmark_functions import Rosenbrock, Ackley
 
 
 class TestRosenbrock(unittest.TestCase):
-    """ Tests for the synthetic benchmark functions """
 
     def test_rosenbrock2d(self):
         test2d = Rosenbrock(**{'dimension':2})
@@ -12,6 +11,17 @@ class TestRosenbrock(unittest.TestCase):
     def test_rosenbrock10d(self):
         test2d = Rosenbrock(**{'dimension':10})
         self.assertAlmostEqual(test2d.evaluate([1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]), 0.0)
+
+class TestAckley(unittest.TestCase):
+    """ Tests for the synthetic benchmark functions """
+
+    def test_ackley(self):
+        test2d = Ackley(**{'dimension':2})
+        self.assertAlmostEqual(test2d.evaluate([0.0, 0.0]), 0.0)
+
+    def test_ackley10d(self):
+        test2d = Ackley(**{'dimension':10})
+        self.assertAlmostEqual(test2d.evaluate([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]), 0.0)
 
 
 if __name__ == '__main__':
