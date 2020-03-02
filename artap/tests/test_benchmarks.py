7c41cc593aeef03e3810b79a1bcab8512a84143e
Tamás <orosz.tamas@yahoo.com>
2020-03-02 19:02:02 +0100

    binh and korn function uncommited till the new version is not implemented
    Former-commit-id: f7d8e7ad9885cf184bfb23aad536518a386d6bca [formerly bb1ea9337b9430630d663f99d16f41ec6f961bd6 [formerly b5b10ed1647e226d865fcede1ca519ce533b04f3]]
    Former-commit-id: 8e1d8bf0663d3f8b38d492571d3e867bbb933d41
    Former-commit-id: 2b04e77a724505592885cdf6d2b82e7538d0d0de

--- artap/tests/test_benchmarks.py
+++ artap/tests/test_benchmarks.py
@@ -1,17 +1,18 @@
 import unittest
+from individual import Individual
 from artap.benchmark_functions import Rosenbrock, Ackley, Schwefel, Sphere, ModifiedEasom, Michaelwicz, Perm, Rastrigin, \
-    SixHump, EqualityConstr, Griewank, Schubert, Zakharov, XinSheYang, XinSheYang2, XinSheYang3
+    SixHump, EqualityConstr, Griewank, Schubert, Zakharov, XinSheYang, XinSheYang2, XinSheYang3, Booth
 
 
 class TestRosenbrock(unittest.TestCase):
 
     def test_rosenbrock2d(self):
         test2d = Rosenbrock(**{'dimension': 2})
-        self.assertAlmostEqual(test2d.evaluate([1.0, 1.0]), 0.0)
+        self.assertAlmostEqual(test2d.evaluate(Individual([1.0, 1.0]))[0], 0.0)
 
     def test_rosenbrock10d(self):
         test2d = Rosenbrock(**{'dimension': 10})
-        self.assertAlmostEqual(test2d.evaluate([1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]), 0.0)
+        self.assertAlmostEqual(test2d.evaluate(Individual([1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]))[0], 0.0)
 
 
 class TestAckley(unittest.TestCase):
@@ -19,93 +20,93 @@ class TestAckley(unittest.TestCase):
 
     def test_ackley(self):
         test2d = Ackley(**{'dimension': 2})
-        self.assertAlmostEqual(test2d.evaluate([0.0, 0.0]), 0.0)
+        self.assertAlmostEqual(test2d.evaluate(Individual([0.0, 0.0]))[0], 0.0)
 
     def test_ackley10d(self):
         test2d = Ackley(**{'dimension': 10})
-        self.assertAlmostEqual(test2d.evaluate([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]), 0.0)
+        self.assertAlmostEqual(test2d.evaluate(Individual([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]))[0], 0.0)
 
 
 class TestSphere(unittest.TestCase):
 
     def test_sphere(self):
         test7d = Sphere(**{'dimension': 7})
-        self.assertAlmostEqual(test7d.evaluate(test7d.global_optimum_coords), 0.0)
+        self.assertAlmostEqual(test7d.evaluate(Individual(test7d.global_optimum_coords))[0], 0.0)
 
 
 class TestSchwefel(unittest.TestCase):
 
     def test_schwefel(self):
         test2d = Schwefel(**{'dimension': 2})
-        self.assertAlmostEqual(test2d.evaluate([420.9687, 420.9687]), 0, 4)
+        self.assertAlmostEqual(test2d.evaluate(Individual([420.9687, 420.9687]))[0], 0, 4)
 
         test10d = Schwefel(**{'dimension': 10})
-        self.assertAlmostEqual(test2d.evaluate(test2d.global_optimum_coords), 0, 4)
+        self.assertAlmostEqual(test2d.evaluate(Individual(test2d.global_optimum_coords))[0], 0, 4)
 
 
 class TestModifiedEasom(unittest.TestCase):
 
     def test_measom(self):
         test2d = ModifiedEasom(**{'dimension': 2})
-        self.assertAlmostEqual(test2d.evaluate(test2d.global_optimum_coords), -1., 4)
+        self.assertAlmostEqual(test2d.evaluate(Individual(test2d.global_optimum_coords))[0], -1., 4)
 
 
 class TestMichaelwicz(unittest.TestCase):
 
     def test_micha2d(self):
         test2d = Michaelwicz(**{'dimension': 2})
-        self.assertAlmostEqual(test2d.evaluate(test2d.global_optimum_coords), test2d.global_optimum, 3)
+        self.assertAlmostEqual(test2d.evaluate(Individual(test2d.global_optimum_coords))[0], test2d.global_optimum, 3)
 
 
 class TestPerm(unittest.TestCase):
 
     def test_perm(self):
         test2d = Perm(**{'dimension': 2})
-        self.assertAlmostEqual(test2d.evaluate(test2d.global_optimum_coords), test2d.global_optimum, 3)
+        self.assertAlmostEqual(test2d.evaluate(Individual(test2d.global_optimum_coords))[0], test2d.global_optimum, 3)
 
     def test_perm10(self):
         test2d = Perm(**{'dimension': 10})
-        self.assertAlmostEqual(test2d.evaluate(test2d.global_optimum_coords), test2d.global_optimum, 3)
+        self.assertAlmostEqual(test2d.evaluate(Individual(test2d.global_optimum_coords))[0], test2d.global_optimum, 3)
 
 
 class TestRastrigin(unittest.TestCase):
 
     def test_rastrigin(self):
         test2d = Rastrigin(**{'dimension': 2})
-        self.assertAlmostEqual(test2d.evaluate(test2d.global_optimum_coords), test2d.global_optimum, 3)
+        self.assertAlmostEqual(test2d.evaluate(Individual(test2d.global_optimum_coords))[0], test2d.global_optimum, 3)
 
     def test_rastrigin10(self):
         test2d = Rastrigin(**{'dimension': 10})
-        self.assertAlmostEqual(test2d.evaluate(test2d.global_optimum_coords), test2d.global_optimum, 3)
+        self.assertAlmostEqual(test2d.evaluate(Individual(test2d.global_optimum_coords))[0], test2d.global_optimum, 3)
 
 
 class TestSixhump(unittest.TestCase):
     # only 2d
     def test_sixhump(self):
         test2d = SixHump(**{'dimension': 2})
-        self.assertAlmostEqual(test2d.evaluate(test2d.global_optimum_coords), test2d.global_optimum, 3)
+        self.assertAlmostEqual(test2d.evaluate(Individual(test2d.global_optimum_coords))[0], test2d.global_optimum, 3)
 
 
 class TestEqualityConstr(unittest.TestCase):
 
     def test_equalityconstr(self):
         test2d = EqualityConstr(**{'dimension': 2})
-        self.assertAlmostEqual(test2d.evaluate(test2d.global_optimum_coords), test2d.global_optimum, 3)
+        self.assertAlmostEqual(test2d.evaluate(Individual(test2d.global_optimum_coords))[0], test2d.global_optimum, 3)
 
     def test_equlaityconstr10(self):
         test2d = EqualityConstr(**{'dimension': 10})
-        self.assertAlmostEqual(test2d.evaluate(test2d.global_optimum_coords), test2d.global_optimum, 3)
+        self.assertAlmostEqual(test2d.evaluate(Individual(test2d.global_optimum_coords))[0], test2d.global_optimum, 3)
 
 
 class TestGriewank(unittest.TestCase):
 
     def test_griewank(self):
         test2d = Griewank(**{'dimension': 2})
-        self.assertAlmostEqual(test2d.evaluate(test2d.global_optimum_coords), test2d.global_optimum, 3)
+        self.assertAlmostEqual(test2d.evaluate(Individual(test2d.global_optimum_coords))[0], test2d.global_optimum, 3)
 
     def test_griewank10(self):
         test2d = Griewank(**{'dimension': 10})
-        self.assertAlmostEqual(test2d.evaluate(test2d.global_optimum_coords), test2d.global_optimum, 3)
+        self.assertAlmostEqual(test2d.evaluate(Individual(test2d.global_optimum_coords))[0], test2d.global_optimum, 3)
 
 
 #class TestSchubert(unittest.TestCase):
@@ -118,42 +119,48 @@ class TestZacharov(unittest.TestCase):
 
     def test_zacharov(self):
         test2d = Zakharov(**{'dimension': 2})
-        self.assertAlmostEqual(test2d.evaluate(test2d.global_optimum_coords), test2d.global_optimum, 3)
+        self.assertAlmostEqual(test2d.evaluate(Individual(test2d.global_optimum_coords))[0], test2d.global_optimum, 3)
 
     def test_griewank10(self):
         test2d = Zakharov(**{'dimension': 10})
-        self.assertAlmostEqual(test2d.evaluate(test2d.global_optimum_coords), test2d.global_optimum, 3)
+        self.assertAlmostEqual(test2d.evaluate(Individual(test2d.global_optimum_coords))[0], test2d.global_optimum, 3)
 
 
 class TestXinSheYang(unittest.TestCase):
 
     def test_xinsheyang(self):
         test2d = XinSheYang(**{'dimension': 2})
-        self.assertAlmostEqual(test2d.evaluate(test2d.global_optimum_coords), test2d.global_optimum, 3)
+        self.assertAlmostEqual(test2d.evaluate(Individual(test2d.global_optimum_coords))[0], test2d.global_optimum, 3)
 
     def test_xinsheyang10(self):
         test2d = XinSheYang(**{'dimension': 10})
-        self.assertAlmostEqual(test2d.evaluate(test2d.global_optimum_coords), test2d.global_optimum, 3)
+        self.assertAlmostEqual(test2d.evaluate(Individual(test2d.global_optimum_coords))[0], test2d.global_optimum, 3)
 
 class TestXinSheYang2(unittest.TestCase):
 
     def test_xinsheyang22(self):
         test2d = XinSheYang2(**{'dimension': 2})
-        self.assertAlmostEqual(test2d.evaluate(test2d.global_optimum_coords), test2d.global_optimum, 3)
+        self.assertAlmostEqual(test2d.evaluate(Individual(test2d.global_optimum_coords))[0], test2d.global_optimum, 3)
 
     def test_xinsheyang210(self):
         test2d = XinSheYang2(**{'dimension': 10})
-        self.assertAlmostEqual(test2d.evaluate(test2d.global_optimum_coords), test2d.global_optimum, 3)
+        self.assertAlmostEqual(test2d.evaluate(Individual(test2d.global_optimum_coords))[0], test2d.global_optimum, 3)
 
 class TestXinSheYang3(unittest.TestCase):
 
     def test_xinsheyang32(self):
         test2d = XinSheYang3(**{'dimension': 2})
-        self.assertAlmostEqual(test2d.evaluate(test2d.global_optimum_coords), test2d.global_optimum, 3)
+        self.assertAlmostEqual(test2d.evaluate(Individual(test2d.global_optimum_coords))[0], test2d.global_optimum, 3)
 
     def test_xinsheyang310(self):
         test2d = XinSheYang3(**{'dimension': 10})
-        self.assertAlmostEqual(test2d.evaluate(test2d.global_optimum_coords), test2d.global_optimum, 3)
+        self.assertAlmostEqual(test2d.evaluate(Individual(test2d.global_optimum_coords))[0], test2d.global_optimum, 3)
+
+class TestBooth(unittest.TestCase):
+
+    def test_booth(self):
+        test2d = Booth()
+        self.assertAlmostEqual(test2d.evaluate(Individual(test2d.global_optimum_coords))[0], test2d.global_optimum, 3)
 
 
 if __name__ == '__main__':
--- artap/tests/test_problem_bayes.py
+++ artap/tests/test_problem_bayes.py
@@ -4,8 +4,6 @@ from artap.problem import Problem
 
 from artap.algorithm_bayes import Bayes, BayesianOptimization
 
-from artap.benchmark_functions import Booth
-
 import numpy as np
 
 
@@ -21,29 +19,29 @@ def target_func(x):
     return val
 
 
-class MyProblemOne(Problem):
-    """ Describe simple one objective optimization problem. """
+# class MyProblemOne(Problem):
+#     """ Describe simple one objective optimization problem. """
+#
+#     def set(self):
+#         self.name = "TestBayesOptParallel"
+#         self.parameters = [{'name': 'x_1', 'initial_value': 2.5, 'bounds': [-2, 10]}]
+#         self.costs = [{'name': 'F'}]
+#
+#     def evaluate(self, individual):
+#         x = individual.vector
+#         return -(np.exp(-(x - 2)**2) + np.exp(-(x - 6)**2/10) + 1 / (x**2 + 1))
 
-    def set(self):
-        self.name = "TestBayesOptParallel"
-        self.parameters = [{'name': 'x_1', 'initial_value': 2.5, 'bounds': [-2, 10]}]
-        self.costs = [{'name': 'F'}]
 
-    def evaluate(self, individual):
-        x = individual.vector
-        return -(np.exp(-(x - 2)**2) + np.exp(-(x - 6)**2/10) + 1 / (x**2 + 1))
-
-
-class MyProblem(Problem):
-    """ Describe simple one objective optimization problem. """
-    def set(self):
-        self.name = "TestBayesOptSerial"
-        self.parameters = [{'name': 'x_1', 'initial_value': 2.5, 'bounds': [-10, 10]},
-                      {'name': 'x_2', 'initial_value': 1.5, 'bounds': [-10, 10]}]
-        self.costs = [{'name': 'F'}]
-
-    def evaluate(self, individual):
-        return [Booth.eval(individual.vector)]
+# class MyProblem(Problem):
+#     """ Describe simple one objective optimization problem. """
+#     def set(self):
+#         self.name = "TestBayesOptSerial"
+#         self.parameters = [{'name': 'x_1', 'initial_value': 2.5, 'bounds': [-10, 10]},
+#                       {'name': 'x_2', 'initial_value': 1.5, 'bounds': [-10, 10]}]
+#         self.costs = [{'name': 'F'}]
+#
+#     def evaluate(self, individual):
+#         return [Booth.eval(individual.vector)]
 
 
 class TestBayesOptimization(unittest.TestCase):
