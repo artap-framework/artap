8781cf6ff1dccc3c69babf31431387c9e61d8717
Tamás <orosz.tamas@yahoo.com>
2020-03-12 16:56:10 +0100

    pareto benchmarks:
    - dtlz ii
    - dtlz iii
    
    
    Former-commit-id: a7849ea0df47a13b30b478f2055dbbd34b089bc8 [formerly 045037c0372890f99c6e7f5b85df29e5cc94c21d [formerly 032b13bc81d3f4200fdc174e932a380a6d4d1a47]]
    Former-commit-id: 112add5ff5206514cb7c0ac7e59dea65a2bb0ba2
    Former-commit-id: 3ddb02ef4603d68d4add154d1219c0aea937e6c0

--- artap/benchmark_pareto.py
+++ artap/benchmark_pareto.py
@@ -1,5 +1,5 @@
 from artap.benchmark_functions import BenchmarkFunction
-from math import cos, pi
+from math import cos, pi, sin
 
 
 class BiObjectiveTestProblem(BenchmarkFunction):
@@ -61,13 +61,14 @@ class DTLZI(BenchmarkFunction):
         \hfill\cr {\rm Minimize}& f_{M}({\rm x})={1\over 2}(1-x_{1})(1+g({\rm x}_{M})),
         \hfill\cr {\rm Subject}\,{\rm to}& 0\leq x_{i}, \leq 1, \quad {\rm for}
         i=1,2, \ldots, n.\hfill}\right\}\eqno{\hbox{(7)}}
-        References
-    ----------
 
-    [1] Deb, K., Thiele, L., Laumanns, M., & Zitzler, E. (2002, May).
-        Scalable multi-objective optimization test problems.
-        In Proceedings of the 2002 Congress on Evolutionary Computation.
-        CEC'02 (Cat. No. 02TH8600) (Vol. 1, pp. 825-830). IEEE.
+        References
+        ----------
+
+        [1] Deb, K., Thiele, L., Laumanns, M., & Zitzler, E. (2002, May).
+            Scalable multi-objective optimization test problems.
+            In Proceedings of the 2002 Congress on Evolutionary Computation.
+            CEC'02 (Cat. No. 02TH8600) (Vol. 1, pp. 825-830). IEEE.
     """
 
     def set(self, **kwargs):
@@ -92,25 +93,155 @@ class DTLZI(BenchmarkFunction):
 
     def evaluate(self, x):
 
-        k = 1  # k >= 1, it can be k = 5 is the offered value,
+        k = 5  # k >= 1, it can be k = 5 is the offered value,
 
         m = len(self.costs)
         x = x.vector
         scores = []
         for i in range(0, m):
             fi = 0.5
-            print('i:', i)
-            for j in range(0, m - i - k):
+            for j in range(0, m - i - 1):
                 fi *= x[j]
 
             if i>0:
-                print(m - i - k)
-                fi *= (1. - x[m - i - k])
+                fi *= (1. - x[m - i - 1])
 
             # g(xm)
             gm = float(k)
             for i in range(0, k):
                 gm += (x[len(x) - i-1] - 0.5) ** 2. - cos(20. * pi * (x[len(x) - i-1] - 0.5))
+            fi = fi * (1 + 100. * gm)
+            scores.append(fi)
+
+        return scores
+
+
+class DTLZII(BenchmarkFunction):
+    """
+    The number of dimensions should be n = m + k -1, where m is the number of the objectives and
+    k is an arbitrary number, which should be bigger than 1.
+
+    .. math::
+        \matrix{ {\rm Minimize} & fi({\rm x})=(1+g({\rm x}_{M}))\cos(x_{1}\pi/2)\cdots\cos(x_{M-1}\pi/2),
+        \hfill\cr {\rm Minimize} & f_{2}({\rm x})=(1+g({\rm x}_{M}))\cos(x_{1}\pi/2)\cdots\sin(x_{M-1}\pi/2),
+        \hfill\cr \vdots & \vdots \hfill\cr {\rm Minimize} & f_{M}({\rm x})=(1+g({\rm x}_{M}))\sin(x_{1}\pi/2),
+        \hfill\cr & 0\leq x_{i}\leq 1,\quad {\rm for} i=1,2, \ldots, n,
+        \hfill\cr {\rm where} & g({\rm x}_{M})=\sum_{x_{i}\in {\rm x}_{M}}(x_{i}-0.5)^{2}.\hfill}\eqno{\hbox{(9)}}
+
+
+        References
+        ----------
+
+        [1] Deb, K., Thiele, L., Laumanns, M., & Zitzler, E. (2002, May).
+            Scalable multi-objective optimization test problems.
+            In Proceedings of the 2002 Congress on Evolutionary Computation.
+            CEC'02 (Cat. No. 02TH8600) (Vol. 1, pp. 825-830). IEEE.
+    """
+
+    def set(self, **kwargs):
+        self.name = 'DTLZ II Test Problem'
+
+        self.set_dimension(**kwargs)
+        self.parameters = self.generate_paramlist(self.dimension, lb=0.0, ub=1.0)
+
+        self.global_optimum = [0.5 for x in range(self.dimension)]  ###
+        self.global_optimum_coords = [0.5 for x in range(self.dimension)]
+
+        # single objective problem
+        self.costs = self.generate_objective_functions(**kwargs)
+
+    def pareto_front(self, x):
+        """ x -> y """
+
+        p_f = []
+        for i in range(0, len(self.costs)):
+            p_f.append(0.5)
+        return p_f
+
+    def evaluate(self, x):
+
+        k = 10  # k >= 1, it can be k = 5 is the offered value,
+
+        m = len(self.costs)
+        x = x.vector
+        scores = []
+        for i in range(0, m):
+            fi = 1.0
+            for j in range(0, m-i-1 ):
+                fi *= cos(0.5*x[j]*pi)
+
+            if i>0:
+                fi *= sin(x[m - i ]*pi/2.)
+            gm = 0.
+            for i in range(0, k):
+                gm += (x[len(x) - i-1] - 0.5) ** 2.
+            fi *= (1. +  gm)
+            scores.append(fi)
+
+        return scores
+
+
+
+class DTLZIII(BenchmarkFunction):
+    """
+    This is the same function like DTLZ II, with the g function of the DTLZ I problem.
+
+    The number of dimensions should be n = m + k -1, where m is the number of the objectives and
+    k is an arbitrary number, which should be bigger than 1.
+
+
+        References
+        ----------
+
+        [1] Deb, K., Thiele, L., Laumanns, M., & Zitzler, E. (2002, May).
+            Scalable multi-objective optimization test problems.
+            In Proceedings of the 2002 Congress on Evolutionary Computation.
+            CEC'02 (Cat. No. 02TH8600) (Vol. 1, pp. 825-830). IEEE.
+    """
+
+    def set(self, **kwargs):
+        self.name = 'DTLZ III Test Problem'
+
+        self.set_dimension(**kwargs)
+        self.parameters = self.generate_paramlist(self.dimension, lb=0.0, ub=1.0)
+
+        self.global_optimum = [0.5 for x in range(self.dimension)]  ###
+        self.global_optimum_coords = [0.5 for x in range(self.dimension)]
+
+        # single objective problem
+        self.costs = self.generate_objective_functions(**kwargs)
+
+    def pareto_front(self, x):
+        """ x -> y """
+
+        p_f = []
+        for i in range(0, len(self.costs)):
+            p_f.append(0.5)
+        return p_f
+
+    def evaluate(self, x):
+
+        k = 10  # k >= 1, it can be k = 5 is the offered value,
+
+        m = len(self.costs)
+        x = x.vector
+        scores = []
+        for i in range(0, m):
+            fi = 1.0
+            for j in range(0, m-i-1 ):
+                fi *= cos(0.5*x[j]*pi)
+
+            if i>0:
+                fi *= sin(x[m - i ]*pi/2.)
+            # gm = 0.
+            # for i in range(0, k):
+            #     gm += (x[len(x) - i-1] - 0.5) ** 2.
+            # fi *= (1. +  gm)
+            # scores.append(fi)
+            # g(xm)
+            gm = float(k)
+            for i in range(0, k):
+                gm += (x[len(x) - i - 1] - 0.5) ** 2. - cos(20. * pi * (x[len(x) - i - 1] - 0.5))
             fi = fi * (1 + 100. * gm)
             scores.append(fi)
 
