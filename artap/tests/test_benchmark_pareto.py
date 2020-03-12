51b674bb305758084a3f19745a71e1f5388c9693
Tamás <orosz.tamas@yahoo.com>
2020-03-12 17:02:16 +0100

    pareto benchmarks:
    - dtlz iv
    
    
    Former-commit-id: c9ed5832a682d143ecf30643217e53b3ae70a526 [formerly 58b66850e2395434d16422c0ebf8baccf969f8fe [formerly da365217c55a82c4cf41e977fda7914b18f78466]]
    Former-commit-id: 0c14bf73631d4237b260bb30aa1542a1d1829cb0
    Former-commit-id: f39d8d8f8c4b0a6e14fd45347e6f0e35c71ce4f6

--- artap/tests/test_benchmark_pareto.py
+++ artap/tests/test_benchmark_pareto.py
@@ -1,6 +1,6 @@
 import unittest
 from artap.individual import Individual
-from artap.benchmark_pareto import BiObjectiveTestProblem, DTLZI,DTLZII, DTLZIII
+from artap.benchmark_pareto import BiObjectiveTestProblem, DTLZI,DTLZII, DTLZIII, DTLZIV
 
 class TestBiobjective(unittest.TestCase):
 
@@ -43,6 +43,21 @@ class TestDTLZIII(unittest.TestCase):
 
     def test_dtlzIII(self):
         test2d = DTLZIII(**{'dimension': 12, 'm':3})
+
+        # The optimal values of this function are on a hyperplane
+
+        x = [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]
+        f0 = test2d.evaluate(Individual(x))[0]
+        f1 = test2d.evaluate(Individual(x))[1]
+        f2 = test2d.evaluate(Individual(x))[2]
+
+        self.assertAlmostEqual(f0**2.0+f1**2.0+f2**2.0, 1.0)
+
+
+class TestDTLZIV(unittest.TestCase):
+
+    def test_dtlzIV(self):
+        test2d = DTLZIV(**{'dimension': 12, 'm':3})
 
         # The optimal values of this function are on a hyperplane
 
