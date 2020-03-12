045037c0372890f99c6e7f5b85df29e5cc94c21d
Tamás <orosz.tamas@yahoo.com>
2020-03-12 16:56:10 +0100

    pareto benchmarks:
    - dtlz ii
    - dtlz iii
    
    
    Former-commit-id: 032b13bc81d3f4200fdc174e932a380a6d4d1a47

--- artap/tests/test_benchmark_pareto.py
+++ artap/tests/test_benchmark_pareto.py
@@ -1,6 +1,6 @@
 import unittest
 from artap.individual import Individual
-from artap.benchmark_pareto import BiObjectiveTestProblem, DTLZI
+from artap.benchmark_pareto import BiObjectiveTestProblem, DTLZI,DTLZII, DTLZIII
 
 class TestBiobjective(unittest.TestCase):
 
@@ -12,16 +12,43 @@ class TestBiobjective(unittest.TestCase):
 class TestDTLZI(unittest.TestCase):
 
     def test_dtlzI(self):
-        test2d = DTLZI(**{'dimension': 4, 'm':3})
+        test2d = DTLZI(**{'dimension': 8, 'm':3})
 
         # The optimal values of this function are on a hyperplane
-
-        f0 = test2d.evaluate(Individual([0.5, 0.5, 0.5, 0.5]))[0]
-        f1 = test2d.evaluate(Individual([0.5, 0.5, 0.5, 0.5]))[1]
-        f2 = test2d.evaluate(Individual([0.5, 0.5, 0.5, 0.5]))[2]
+        x = [0.5, 0.5, 0.5, 0.5,0.5, 0.5, 0.5, 0.5]
+        f0 = test2d.evaluate(Individual(x))[0]
+        f1 = test2d.evaluate(Individual(x))[1]
+        f2 = test2d.evaluate(Individual(x))[2]
 
         self.assertAlmostEqual(f0+f1+f2, 0.5)
 
 
+class TestDTLZII(unittest.TestCase):
+
+    def test_dtlzII(self):
+        test2d = DTLZII(**{'dimension': 12, 'm':3})
+
+        # The optimal values of this function are on a hyperplane
+
+        x = [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]
+        f0 = test2d.evaluate(Individual(x))[0]
+        f1 = test2d.evaluate(Individual(x))[1]
+        f2 = test2d.evaluate(Individual(x))[2]
+
+        self.assertAlmostEqual(f0**2.0+f1**2.0+f2**2.0, 1.0)
 
 
+
+class TestDTLZIII(unittest.TestCase):
+
+    def test_dtlzIII(self):
+        test2d = DTLZIII(**{'dimension': 12, 'm':3})
+
+        # The optimal values of this function are on a hyperplane
+
+        x = [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]
+        f0 = test2d.evaluate(Individual(x))[0]
+        f1 = test2d.evaluate(Individual(x))[1]
+        f2 = test2d.evaluate(Individual(x))[2]
+
+        self.assertAlmostEqual(f0**2.0+f1**2.0+f2**2.0, 1.0)
