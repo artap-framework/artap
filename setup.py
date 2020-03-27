87382e5520f8641d73dbd96dfc42bcc3bfa5cb14
Tamás <orosz.tamas@yahoo.com>
2020-03-27 01:30:25 +0100

    setup.py -- numpy installed by kwargs --- smt not works properly
    requirements.txt -- optproblems removed
    
    README.md -- updated
    
    upload script:
    -------------
    --skip-existing keyword was removed, because sometimes it not updates the files if it was existing in the previous version
    
    
    Former-commit-id: ce2b50c59b4f5b2661c2f5faac902f3dd0bb62fe

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-import setuptools
+from setuptools import Command, setup, find_packages
 from setuptools.command.install import install
 import subprocess
 import os
@@ -24,29 +24,50 @@ with open("README.md", "r") as fh:
 with open("requirements.txt", "r") as fh:
     requirements = fh.read().split("\n")
 
-setuptools.setup(
-    name="artap",
-    version="2020.3.21.1",
-    author=u"Artap Team",
-    author_email="artap.framework@gmail.com",
-    description="Platform for robust design optimization",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="http://www.agros2d.org/artap/",
-    python_requires='>3.7',
-    license="License :: OSI Approved :: MIT License",
-    cmdclass={'install': CustomInstallCommand},
-    packages=setuptools.find_packages(),
-    include_package_data=True,
-    # data_files=[('artap/lib', ['artap/lib/bayesopt.so']),
-    install_requires=requirements,
-    # scripts=['3rdparty/submodules.sh'],
-    classifiers=[
-        "Intended Audience :: Science/Research",
-        "Operating System :: POSIX :: Linux",
-        "Topic :: Scientific/Engineering",
-        'Programming Language :: Python :: 3.6',
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-)
+min_numpy_ver = "1.13.3"
+
+
+def setup_package():
+    setuptools_kwargs = {
+        "install_requires": [
+            # "python-dateutil >= 2.6.1",
+            # "pytz >= 2017.2",
+            f"numpy >= {min_numpy_ver}",
+            requirements,
+            "smt >= 0.3.4",
+        ],
+        "setup_requires": [f"numpy >= {min_numpy_ver}"],
+        "zip_safe": False,
+    }
+
+    setup(
+        name="artap",
+        version="2020.3.26.8",
+        author=u"Artap Team",
+        author_email="artap.framework@gmail.com",
+        description="Platform for robust design optimization",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="http://www.agros2d.org/artap/",
+        python_requires='>=3.7',
+        license="License :: OSI Approved :: MIT License",
+        cmdclass={'install': CustomInstallCommand},
+        packages=find_packages(),
+        include_package_data=True,
+        # data_files=[('artap/lib', ['artap/lib/bayesopt.so']),
+        # install_requires=requirements,
+        # scripts=['3rdparty/submodules.sh'],
+        classifiers=[
+            "Intended Audience :: Science/Research",
+            "Operating System :: POSIX :: Linux",
+            "Topic :: Scientific/Engineering",
+            'Programming Language :: Python :: 3.7',
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+        **setuptools_kwargs,
+    )
+
+
+if __name__ == "__main__":
+    setup_package()
