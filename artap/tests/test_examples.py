b8f3431c3a3a2f272d2777767e035c165a04d187
Pavel Karban <karban@kte.zcu.cz>
2020-03-28 08:51:24 +0100

    improved test examples (split to particular tests)
    Former-commit-id: cfba4c7c078ad13652e3dd5f36918350029bb3cd [formerly d882b4c48222aa28bea610130da5f37a6873d9e2 [formerly 78d5f3bf22532568b36b7f319b2f128ebf0e0a2e]]
    Former-commit-id: ca12320040ad1184ea404d743fd7ca7ce33f6d3d
    Former-commit-id: eaff65893cf8ddabfa49529979f2135b7f4b2ccd

--- artap/tests/test_examples.py
+++ artap/tests/test_examples.py
@@ -3,20 +3,43 @@ import importlib.util
 import os
 
 
+def make_test_function(name, filename):
+    def test(self):
+        print(name)
+        spec = importlib.util.spec_from_file_location(name, filename)
+        script = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(script)
+    return test
+
+
 class TestExamples(unittest.TestCase):
-    """ Tests simple one objective optimization problem."""
-
-    def test_all_examples(self):
-
+    """ Tests all examples in examples directory."""
+    @staticmethod
+    def init_tests():
         path = os.getcwd() + '/../examples/'
         for module in os.listdir(path):
             if module == '__init__.py' or module[-3:] != '.py':
                 continue
-            spec = importlib.util.spec_from_file_location(module[:-3], path + os.sep + module)
-            script = importlib.util.module_from_spec(spec)
-            spec.loader.exec_module(script)
-        del module
 
+            name = module[:-3]
+            filename = path + os.sep + module
+            test_func = make_test_function(name, filename)
+            setattr(TestExamples, 'test_{0}'.format(name), test_func)
+
+    # def test_all_examples(self):
+    #
+    #     path = os.getcwd() + '/../examples/'
+    #     for module in os.listdir(path):
+    #         if module == '__init__.py' or module[-3:] != '.py':
+    #             continue
+    #         spec = importlib.util.spec_from_file_location(module[:-3], path + os.sep + module)
+    #         script = importlib.util.module_from_spec(spec)
+    #         spec.loader.exec_module(script)
+    #     del module
+
+
+# create tests dynamically
+TestExamples.init_tests()
 
 if __name__ == '__main__':
     unittest.main()
