b6d86ff3f24ed97c2548d647154c4f37c96efcb3
David Pánek <panek50@kte.zcu.cz>
2020-04-30 11:06:07 +0200

    fixing examples minor corrections
    Former-commit-id: fbebe4312e3157543790be9885415d2866a66c65

--- artap/examples/test_examples.py
+++ artap/examples/test_examples.py
@@ -3,6 +3,15 @@ import importlib.util
 import os
 
 
+try:
+    from agrossuite import agros as a2d
+
+    __agros__ = True
+except ImportError:
+    print("Agros is not present test skipped")
+    __agros__ = False
+
+
 def make_test_function(name, filename):
     def test(self):
         spec = importlib.util.spec_from_file_location(name, filename)
@@ -23,6 +32,8 @@ class TestExamples(unittest.TestCase):
         path = os.getcwd()
         for folder in os.walk(path):
             if folder[0].split('/')[-1] =='__pycache__':
+                continue
+            if folder[0].find('agros') > 0 and not (__agros__):
                 continue
             # print(folder)
             files = folder[2]
