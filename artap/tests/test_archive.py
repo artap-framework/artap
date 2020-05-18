e120c5a31d98740ca9444a66b1790989ba872adc
Tamás <orosz.tamas@yahoo.com>
2020-05-18 15:33:19 +0200

    - testing the archive class: - size operator added to the archive class
    Former-commit-id: 32646be7e3b59220584468f6e6b4016d201e8542

--- artap/tests/test_archive.py
+++ artap/tests/test_archive.py
@@ -1,4 +1,5 @@
 from archive import Archive
+from artap.individual import Individual
 
 import unittest
 
@@ -10,3 +11,13 @@ class TestArchive(unittest.TestCase):
 
     def test_should_constructor_create_a_non_null_object(self):
         self.assertIsNotNone(self.archive)
+
+    def test_should_adding_one_solution_work_properly(self):
+
+        x = Individual([2, 2])
+        x.costs = [-1.0, 5.0, 9.0]
+        x.costs_signed = [-1.0, 5.0, 9.0, 0]
+
+        self.archive.add(x)
+        self.assertEqual(1, self.archive.size())
+        self.assertEqual(x, self.archive._contents[0])
