0459c65d2ab06714074cb9ccc7187877c0f89895
Tamás <orosz.tamas@yahoo.com>
2020-05-18 21:17:35 +0200

    - testing the archive class: - add operator rewritten, based on jmetalpy and the original idea, platypus didn't examined that the individual is already in the set and it iterated through the list two times, but in a more efficient way by generator expressions
    Former-commit-id: d52d033db6ba35650d87fa4486fb75ffdefb92a5 [formerly 40065222a0d880f86ab2ba45d5978de8b8d98731 [formerly 99cf6367bff52902701293920392f9a0da2f9fb2]]
    Former-commit-id: 847e8f3c48648811e0288fe36b05ce1937962e98
    Former-commit-id: 0e9c7dfb84086d6d1dc388ba7b7b9041cd275125

--- artap/archive.py
+++ artap/archive.py
@@ -9,27 +9,56 @@ class Archive(object):
 
     def __init__(self, dominance=ParetoDominance):
         super(Archive, self).__init__()
-        self._dominance = dominance  # dominance comparator
+        self._dominance = dominance()  # dominance comparator
         self._contents = []
 
     def add(self, individual):
         """
-        
         :param individual: 
         :return: 
         """
-        flags = [self._dominance.compare(individual, s) for s in self._contents]
-        dominates = [x > 0 for x in flags]
-        nondominated = [x == 0 for x in flags]
 
-        if any(dominates):
-            return False
+        is_dominated = False
+        is_contained = False
+
+        if len(self._contents) == 0:
+            self._contents.append(individual)
+            return True
         else:
-            self._contents = list(itertools.compress(self._contents, nondominated)) + [individual]
+            number_of_deleted_solutions = 0
+
+            # New copy of list and enumerate
+            for index, current_solution in enumerate(list(self._contents)):
+                is_dominated_flag = self._dominance.compare(individual.costs_signed, current_solution.costs_signed)
+                if is_dominated_flag == 1:
+                    del self._contents[index - number_of_deleted_solutions]
+                    number_of_deleted_solutions += 1
+                elif is_dominated_flag == 2:
+                    is_dominated = True
+                    break
+                elif is_dominated_flag == 0:
+                    if individual.costs_signed == current_solution.costs_signed:
+                        is_contained = True
+                        break
+
+        if not is_dominated and not is_contained:
+            self._contents.append(individual)
             return True
 
-    def append(self, solution):
-        self.add(solution)
+        return False
+
+        # flags = [self._dominance.compare(individual.costs_signed, s.costs_signed) for s in self._contents]
+        # dominates = [x == 2 for x in flags]
+        # nondominated = [x == 0 for x in flags]
+        #
+        # if any(dominates):
+        #     return False
+        # else:
+        #     self._contents = list(itertools.compress(self._contents, nondominated)) + [individual]
+        #     return True
+
+    def append(self, individual):
+        self.add(individual)
 
     def extend(self, solutions):
         for solution in solutions:
