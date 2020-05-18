40065222a0d880f86ab2ba45d5978de8b8d98731
Tamás <orosz.tamas@yahoo.com>
2020-05-18 21:17:35 +0200

    - testing the archive class: - add operator rewritten, based on jmetalpy and the original idea, platypus didn't examined that the individual is already in the set and it iterated through the list two times, but in a more efficient way by generator expressions
    Former-commit-id: 99cf6367bff52902701293920392f9a0da2f9fb2

--- artap/tests/test_archive.py
+++ artap/tests/test_archive.py
@@ -13,7 +13,6 @@ class TestArchive(unittest.TestCase):
         self.assertIsNotNone(self.archive)
 
     def test_should_adding_one_solution_work_properly(self):
-
         x = Individual([2, 2])
         x.costs = [-1.0, 5.0, 9.0]
         x.costs_signed = [-1.0, 5.0, 9.0, 0]
@@ -23,7 +22,6 @@ class TestArchive(unittest.TestCase):
         self.assertEqual(x, self.archive._contents[0])
 
     def test_should_adding_two_solutions_work_properly_if_one_is_dominated(self):
-
         dominated_solution = Individual([1, 2])
         dominated_solution.costs = [2.0, 2.0, 0]
         dominated_solution.costs_signed = [2.0, 2.0, 0]
@@ -37,3 +35,67 @@ class TestArchive(unittest.TestCase):
 
         self.assertEqual(1, self.archive.size())
         self.assertEqual(dominant_solution, self.archive._contents[0])
+
+    def test_should_adding_two_solutions_work_properly_if_both_are_non_dominated(self):
+        x = Individual([1, 2])
+        x.costs = [1.0, 0.0, 0]
+        x.costs_signed = [1.0, 0.0, 0]
+
+        y = Individual([1, 1])
+        y.costs = [0.0, 1.0, 0]
+        y.costs_signed = [0.0, 1.0, 0]
+
+        self.archive.add(x)
+        self.archive.add(y)
+
+        self.assertEqual(2, self.archive.size())
+        self.assertTrue(x in self.archive._contents and
+                        y in self.archive._contents)
+
+    def test_should_adding_four_solutions_work_properly_if_one_dominates_the_others(self):
+        x = Individual([1, 2])
+        x.costs = [1.0, 1.0]
+        x.costs_signed = [1.0, 1.0, 0]
+
+        y = Individual([1, 2])
+        y.costs = [0.0, 2.0]
+        y.costs_signed = [0.0, 2.0, 0]
+
+        z = Individual([1, 2])
+        z.costs = [0.5, 1.5]
+        z.costs_signed = [0.5, 1.5, 0]
+
+        v = Individual([1, 2])
+        v.costs = [0.0, 0.0]
+        v.costs_signed = [0.0, 0.0, 0]
+
+        self.archive.add(x)
+        self.archive.add(y)
+        self.archive.add(z)
+        self.archive.add(v)
+
+        self.assertEqual(1, self.archive.size())
+        self.assertEqual(v, self.archive._contents[0])
+
+    def test_should_adding_three_solutions_work_properly_if_two_of_them_are_equal(self):
+        x = Individual([1, 2])
+        x.costs = [1.0, 1.0]
+        x.costs_signed = [1.0, 1.0, 0.0]
+
+        y = Individual([1, 2])
+        y.costs = [0.0, 2.0]
+        y.costs_signed = [0.0, 2.0, 0.0]
+
+        z = Individual([1, 2])
+        z.costs = [1.0, 1.0]
+        z.costs_signed = [1.0, 1.0, 0.0]
+
+        self.archive.add(x)
+        self.archive.add(y)
+
+        result = self.archive.add(z)
+
+        self.assertEqual(2, self.archive.size())
+        self.assertFalse(result)
+        self.assertTrue(x in self.archive._contents
+                        or y in self.archive._contents)
