0c774510cdb817fd28ab84991012b7784a3a3156
Tamás <orosz.tamas@yahoo.com>
2020-05-31 02:43:09 +0200

    swarm base class is introduced
    omopso:
     - speed constriction is introduced and tested
     - constriction factor is introduced
    
    Former-commit-id: 02bdf9de76fc7e1ad8bf4e04e8358f11f6976e3c

--- artap/tests/test_problem_swarm.py
+++ artap/tests/test_problem_swarm.py
@@ -45,49 +45,6 @@ class TestAckleyv1(unittest.TestCase):
             self.test_local_problem(int(1.5 * population_number))
 
 
-# Very poor performance
-
-# class TestAlpine(unittest.TestCase):
-#
-#
-#     def test_local_problem(self, population_number=30):
-#         try:
-#             problem = AlpineFunction(**{'dimension': 3})
-#             algorithm = PSO(problem)
-#             algorithm.options['max_population_number'] = population_number
-#             algorithm.options['max_population_size'] = 100
-#             algorithm.options['max_processes'] = 3
-#             algorithm.run()
-#
-#             b = Results(problem)
-#             optimum = b.find_minimum('F_1')  # Takes last cost function
-#             self.assertAlmostEqual(optimum.costs[0], problem.global_optimum, 1)
-#         except AssertionError:
-#             # stochastic
-#             print("TestAlpine::test_local_problem", population_number)
-#             self.test_local_problem(int(1.5 * population_number))
-
-
-# class TestAlpinev1(unittest.TestCase):
-#
-#
-#     def test_local_problem(self, population_number=30):
-#         try:
-#             problem = AlpineFunction(**{'dimension': 3})
-#             algorithm = PSO(problem)
-#             algorithm.options['max_population_number'] = population_number
-#             algorithm.options['max_population_size'] = 100
-#             algorithm.options['max_processes'] = 3
-#             algorithm.run()
-#
-#             b = Results(problem)
-#             optimum = b.find_minimum('F_1')  # Takes last cost function
-#             self.assertAlmostEqual(optimum.costs[0], problem.global_optimum, 1)
-#         except AssertionError:
-#             # stochastic
-#             print("TestAlpine::test_local_problem", population_number)
-#             self.test_local_problem(int(1.5 * population_number))
-#
 
 
 if __name__ == '__main__':
