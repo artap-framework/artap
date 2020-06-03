940a6b4c20eb05bdb8ae90c437ac142ed15f1328
Tamás <orosz.tamas@yahoo.com>
2020-06-03 18:42:27 +0200

    swarm base class is introduced
    omopso:
     - speed constriction is introduced and tested
     - constriction factor is introduced
    
    archive:
     - extended by a fittness truncate function, this can order and reduce the number of the items as an arbitrary number.
     - fittness truncate function of the archive is tested
     - turbulence is works and tested
     - particle best initilatization works, tested
     - particle best update works, tested
    
    Former-commit-id: 1a723c49d2325beda7fdb63e97799b347624edae

--- artap/tests/test_problem_swarm.py
+++ artap/tests/test_problem_swarm.py
@@ -7,13 +7,14 @@ from artap.benchmark_functions import BinhAndKorn, AlpineFunction, Ackley
 class TestAckley(unittest.TestCase):
     """ Tests that the PSO can find the global optimum. """
 
-    def test_local_problem(self, population_number=30):
+    def test_local_problem(self, population_number=50):
         try:
             problem = Ackley(**{'dimension': 1})
             algorithm = OMOPSO(problem)
             algorithm.options['max_population_number'] = population_number
-            algorithm.options['max_population_size'] = 30
-            algorithm.options['max_processes'] = 10
+            algorithm.options['max_population_size'] = 100
+            algorithm.options['max_processes'] = 1
+            algorithm.options['epsilons'] = 0.01
             algorithm.run()
 
             b = Results(problem)
@@ -24,25 +25,25 @@ class TestAckley(unittest.TestCase):
             print("TestAckleyN222::test_local_problem", population_number)
             self.test_local_problem(int(1.5 * population_number))
 
-class TestAckleyv1(unittest.TestCase):
-    """ Tests that the PSO can find the global optimum. """
-
-    def test_local_problem(self, population_number=30):
-        try:
-            problem = Ackley(**{'dimension': 1})
-            algorithm = PSO_V1(problem)
-            algorithm.options['max_population_number'] = population_number
-            algorithm.options['max_population_size'] = 30
-            algorithm.options['max_processes'] = 10
-            algorithm.run()
-
-            b = Results(problem)
-            optimum = b.find_optimum('F_1')  # Takes last cost function
-            self.assertAlmostEqual(optimum.costs[0], problem.global_optimum, 1)
-        except AssertionError:
-            # stochastic
-            print("TestAckleyN222::test_local_problem", population_number)
-            self.test_local_problem(int(1.5 * population_number))
+# class TestAckleyv1(unittest.TestCase):
+#     """ Tests that the PSO can find the global optimum. """
+#
+#     def test_local_problem(self, population_number=30):
+#         try:
+#             problem = Ackley(**{'dimension': 1})
+#             algorithm = PSO_V1(problem)
+#             algorithm.options['max_population_number'] = population_number
+#             algorithm.options['max_population_size'] = 30
+#             algorithm.options['max_processes'] = 10
+#             algorithm.run()
+#
+#             b = Results(problem)
+#             optimum = b.find_optimum('F_1')  # Takes last cost function
+#             self.assertAlmostEqual(optimum.costs[0], problem.global_optimum, 1)
+#         except AssertionError:
+#             # stochastic
+#             print("TestAckleyN222::test_local_problem", population_number)
+#             self.test_local_problem(int(1.5 * population_number))
 
 
 
--- artap/tests/test_swarm.py
+++ artap/tests/test_swarm.py
@@ -1,13 +1,29 @@
 import unittest
 from artap.benchmark_functions import Ackley
-from artap.algorithm_swarm import SwarmAlgorithm
+from artap.algorithm_swarm import SwarmAlgorithm, OMOPSO
 from artap.problem import Problem
+from artap.individual import Individual
+
+
+class TestProblem(Problem):
+
+    def set(self):
+        self.name = 'Test Problem'
+        self.parameters = [{'name': '1', 'bounds': [2., 10.]},
+                           {'name': '2', 'bounds': [2., 20.]},
+                           ]
+
+        self.costs = [{'name': 'f_1', 'criteria': 'minimize'}]
+
+    def evaluate(self, individual):
+        return [1, 1]
 
 
 class TestSwarm(unittest.TestCase):
 
     def setUp(self):
-        problem = Problem()
+        problem = TestProblem()
+
         self.Swarm = SwarmAlgorithm(problem)
 
     def test_should_constructor_create_a_valid_object(self):
@@ -29,4 +45,67 @@ class TestSwarm(unittest.TestCase):
     def test_speed_do_not_change(self):
         self.assertEqual(self.Swarm.speed_constriction(0.1, 0., -1.), 0.1)
 
+    def test_particle_best_should_initialized_correctly(self):
+        z = Individual([1, 2])
+        z.costs = [0.5, 1.5]
+        z.costs_signed = [0.5, 1.5, 0]
 
+        z.features = {'velocity': [], 'best_cost': [], 'best_vector': []}  #
+
+        population = [z]
+
+        self.Swarm.init_pbest(population)
+
+        self.assertEqual(z.features['best_cost'], z.costs_signed)
+        self.assertEqual(z.features['best_vector'], z.vector)
+
+    def test_particle_best_update_should_change_the_values(self):
+        z = Individual([1, 2])
+        z.costs = [0.5, 1.5]
+        z.costs_signed = [0.5, 1.5, 0]
+
+        z.features = {'velocity': [1, 2], 'best_cost': [2., 2., 0.], 'best_vector': [1, 2]}
+
+        population = [z]
+
+        self.Swarm.update_particle_best(population)
+
+        self.assertEqual(z.features['best_cost'], z.costs_signed)
+        self.assertEqual(z.features['best_vector'], z.vector)
+
+    def test_particle_best_update_should_not_change_the_values(self):
+        z = Individual([1, 2])
+        z.costs = [0.5, 1.5]
+        z.costs_signed = [0.5, 1.5, 0]
+
+        z.features = {'velocity': [1, 2], 'best_cost': [0., 0., 0.], 'best_vector': [1, 2]}
+
+        population = [z]
+
+        self.Swarm.update_particle_best(population)
+        self.assertNotEqual(z.features['best_cost'], z.costs_signed)
+
+
+class TestSwarm(unittest.TestCase):
+
+    def setUp(self):
+        problem = TestProblem()
+        self.omopso = OMOPSO(problem)
+        self.omopso.mutator.probability = 1.0
+
+    def test_turbulence_should_change_vector(self):
+
+        z = Individual([1, 2])
+        z.costs = [0.5, 1.5]
+        z.costs_signed = [0.5, 1.5, 0]
+
+        z.features = {'velocity': [1, 2], 'best_cost': [0., 0., 0.], 'best_vector': [1, 2]}
+
+        population = [z]
+        self.omopso.turbulence(population)
+
+        # the new population vector should be differ from the original one
+        self.assertNotEqual(population[0].vector, [1,2])
+
+        # it should preserve the features
+        self.assertEqual(population[0].features, z.features)
