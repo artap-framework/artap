1a310402aea7000ff8e209effaadb2c556b506e0
Tamás <orosz.tamas@yahoo.com>
2020-06-05 00:48:34 +0200

    swarm base class is introduced
    omopso:
     - speed constriction is introduced and tested
     - constriction factor is introduced
    
    archive:
     - extended by a fittness truncate function, this can order and reduce the number of the items as an arbitrary number.
     - fittness truncate function of the archive is tested
     - turbulence is works and tested
     - particle best initilatization works, tested
     - particle best update works, tested
     - every function for omopso is tested
     - Ackley function works for OMOPSO
     - uniform mutation
    
    Former-commit-id: 5dc53b5c8a82256850c25f8cbbaf9160a532f65d [formerly 052990e9dce6e567af0397ccef4598d2788b8dc3 [formerly 7bfe9e54e665a1b670dda63bf0382107bf3666e6]]
    Former-commit-id: 23e95538bb39bcc2a089f071c806671dc8785a2b
    Former-commit-id: eeec3625690f79621df272b88e53b821cf99842f

--- artap/algorithm_swarm.py
+++ artap/algorithm_swarm.py
@@ -179,10 +179,20 @@ class OMOPSO(SwarmAlgorithm):
             #     individual.features['max_speed'] = delta_i
         return
 
+    # for SMPSO on 15% of the particles
+    # def turbulence(self, population):
+    #     for i in range(len(population)):
+    #         mutated = self.mutator.mutate(population[i])
+    #         population[i].vector = copy(mutated.vector)
+
     def turbulence(self, population):
-        for i in range(len(population)):
-            mutated = self.mutator.mutate(population[i])
-            population[i].vector = copy(mutated.vector)
+        """
+        OMOPSO applies a combination of uniform and nonuniform
+        mutation to the particle swarm(uniform mutation to the first 30 % of
+        the swarm, non - uniform to the next 30 %, and no mutation on the particles)
+        """
+
+        return
 
     def update_velocity(self, population):
 
@@ -229,8 +239,8 @@ class OMOPSO(SwarmAlgorithm):
 
         for particle in swarm:
             # because the two archive can be very different
-            self.leaders.add(particle)
-            self.problem.archive.add(particle)
+            if self.leaders.add(particle):
+                self.problem.archive.add(particle)
 
         # the length of the leaders archive cannot be longer than the number of the initial population
         self.leaders.truncate(self.population_size, 'crowding_distance')
@@ -290,92 +300,93 @@ class OMOPSO(SwarmAlgorithm):
 
             # self.evaluator.evaluate(offsprings)
             self.evaluate(offsprings)
-            # self.add_features(offsprings) -- do not reset
             population = Population(offsprings)
-            self.problem.populations.append(population)
 
+            # veolcities are stored in features
             self.update_particle_best(offsprings)
             self.update_global_best(offsprings)
+            # self.add_features(offsprings) -- do not reset
+
+            self.problem.populations.append(population)
 
             # population = self.step(population)
             i += i + 1
         t = time.time() - t_s
         self.problem.logger.info("PSO: elapsed time: {} s".format(t))
 
-
 # ........................
 #
 # ........................
 
-class PSO_V1(SwarmAlgorithm):
-    """
-
-    X. Li. A Non-dominated Sorting Particle Swarm Optimizer for Multiobjective
-    Optimization. In Genetic and Evolutionary Computation - GECCO 2003, volume
-    2723 of LNCS, pages 37–48, 2003.
-
-    This algorithm is a variant of the original PSO, published by Eberhart(2000), the origin of this modified algorithm,
-    which constriction factor was introduced by Clercs in 1999.
-
-    The code is based on SHI and EBERHARTS algorithm.
-
-    Empirical study of particle swarm optimization,” in Proc. IEEE Int. Congr. Evolutionary Computation, vol. 3,
-    1999, pp. 101–106.
-    """
-
-    def __init__(self, problem: Problem, name="Particle Swarm Algorithm - with time varieting inertia weight"):
-        super().__init__(problem, name)
-        self.n = self.options['max_population_size']
-        self.mutator = SwarmStepTVIW(self.problem.parameters, self.options['max_population_number'])
-        self.selector = DummySelector(self.problem.parameters, self.problem.signs)
-
-    def run(self):
-        # set random generator
-        self.generator = RandomGenerator(self.problem.parameters)
-        self.generator.init(self.options['max_population_size'])
-
-        population = self.gen_initial_population()
-        self.evaluate(population.individuals)
-        self.add_features(population.individuals)
-
-        for individual in population.individuals:
-            self.mutator.evaluate_best_individual(
-                individual)  # TODO: all evaluating should be derived from Evaluator class
-
-        self.selector.fast_nondominated_sorting(population.individuals)
-        self.problem.populations.append(population)
-
-        t_s = time.time()
-        self.problem.logger.info("PSO: {}/{}".format(self.options['max_population_number'],
-                                                     self.options['max_population_size']))
-
-        i = 0
-        while i < self.options['max_population_number']:
-            offsprings = self.selector.select(population.individuals)
-
-            pareto_front = []
-            for individual in offsprings:
-                if individual.features['front_number'] == 1:
-                    pareto_front.append(individual)
-
-            for individual in offsprings:
-                index = randint(0, len(pareto_front) - 1)  # takes random individual from Pareto front
-                best_individual = pareto_front[index]
-                if best_individual is not individual:
-                    self.mutator.update(best_individual)
-                    self.mutator.mutate(individual)
-
-            self.evaluate(offsprings)
-            self.add_features(offsprings)
-
-            for individual in offsprings:
-                self.mutator.evaluate_best_individual(individual)
-
-            self.selector.fast_nondominated_sorting(offsprings)
-            population = Population(offsprings)
-            self.problem.populations.append(population)
-
-            i += 1
-
-        t = time.time() - t_s
-        self.problem.logger.info("PSO: elapsed time: {} s".format(t))
+# class PSO_V1(SwarmAlgorithm):
+#     """
+#
+#     X. Li. A Non-dominated Sorting Particle Swarm Optimizer for Multiobjective
+#     Optimization. In Genetic and Evolutionary Computation - GECCO 2003, volume
+#     2723 of LNCS, pages 37–48, 2003.
+#
+#     This algorithm is a variant of the original PSO, published by Eberhart(2000), the origin of this modified algorithm,
+#     which constriction factor was introduced by Clercs in 1999.
+#
+#     The code is based on SHI and EBERHARTS algorithm.
+#
+#     Empirical study of particle swarm optimization,” in Proc. IEEE Int. Congr. Evolutionary Computation, vol. 3,
+#     1999, pp. 101–106.
+#     """
+#
+#     def __init__(self, problem: Problem, name="Particle Swarm Algorithm - with time varieting inertia weight"):
+#         super().__init__(problem, name)
+#         self.n = self.options['max_population_size']
+#         self.mutator = SwarmStepTVIW(self.problem.parameters, self.options['max_population_number'])
+#         self.selector = DummySelector(self.problem.parameters, self.problem.signs)
+#
+#     def run(self):
+#         # set random generator
+#         self.generator = RandomGenerator(self.problem.parameters)
+#         self.generator.init(self.options['max_population_size'])
+#
+#         population = self.gen_initial_population()
+#         self.evaluate(population.individuals)
+#         self.add_features(population.individuals)
+#
+#         for individual in population.individuals:
+#             self.mutator.evaluate_best_individual(
+#                 individual)  # TODO: all evaluating should be derived from Evaluator class
+#
+#         self.selector.fast_nondominated_sorting(population.individuals)
+#         self.problem.populations.append(population)
+#
+#         t_s = time.time()
+#         self.problem.logger.info("PSO: {}/{}".format(self.options['max_population_number'],
+#                                                      self.options['max_population_size']))
+#
+#         i = 0
+#         while i < self.options['max_population_number']:
+#             offsprings = self.selector.select(population.individuals)
+#
+#             pareto_front = []
+#             for individual in offsprings:
+#                 if individual.features['front_number'] == 1:
+#                     pareto_front.append(individual)
+#
+#             for individual in offsprings:
+#                 index = randint(0, len(pareto_front) - 1)  # takes random individual from Pareto front
+#                 best_individual = pareto_front[index]
+#                 if best_individual is not individual:
+#                     self.mutator.update(best_individual)
+#                     self.mutator.mutate(individual)
+#
+#             self.evaluate(offsprings)
+#             self.add_features(offsprings)
+#
+#             for individual in offsprings:
+#                 self.mutator.evaluate_best_individual(individual)
+#
+#             self.selector.fast_nondominated_sorting(offsprings)
+#             population = Population(offsprings)
+#             self.problem.populations.append(population)
+#
+#             i += 1
+#
+#         t = time.time() - t_s
+#         self.problem.logger.info("PSO: elapsed time: {} s".format(t))
