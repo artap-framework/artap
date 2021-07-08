1e783944cf6a46393b6a5aa8487b174b6a56988c
Hamid <neghab@fel.zcu.cz>
2021-07-08 15:22:01 +0200

    Unit-test for NNQuantize is added.

--- artap/NNQuantized.py
+++ artap/NNQuantized.py
@@ -1,9 +1,16 @@
 import numpy as np
 import tensorflow as tf
+from scipy.sparse import csr_matrix
 import tensorflow._api.v2.compat as tv
+from keras.layers import InputSpec, Layer, Dense, Conv2D
+from keras import backend as K
+# tv.disable_v2_behavior()
+tv.v1.disable_v2_behavior()
 from keras.models import Sequential
-
-from .problem import Problem
+from matplotlib import pyplot as plt
+from keras import constraints
+from keras import initializers
+from artap.problem import Problem
 
 '''
     paper: Deep Compression: Compressing Deep Neural Networks with Pruning, Trained Quantization and Huffman Coding.
@@ -15,6 +22,7 @@ from .problem import Problem
 class LayerTrain(object):
 
     def __init__(self, in_depth, out_depth, N_clusters, name):
+        self.clusters_masks = []
         self.name = name
         if 'conv' in name:
             self.w = tf.Variable(tf.random.normal([5, 5, in_depth, out_depth], stddev=0.1))
@@ -38,24 +46,25 @@ class LayerTrain(object):
         elif 'fc' in self.name:
             return tf.matmul(x, self.w)
 
-    # def save_weights_histogram(self, sess, directory, iteration):
-    #     w_data = sess.run(self.w).reshape(-1)
-    #     valid_w_data = [x for x in w_data if x != 0.0]
-    #
-    #     plt.grid(True)
-    #     plt.hist(valid_w_data, 100, color='0.4')
-    #     plt.gca().set_xlim([-0.4, 0.4])
-    #     plt.savefig(directory + '/' + self.name + '-' + str(iteration), dpi=100)
-    #     plt.gcf().clear()
+    def save_weights_histogram(self, sess, directory, iteration):
+        w_data = sess.run(self.w).reshape(-1)
+        valid_w_data = [x for x in w_data if x != 0.0]
 
-    # def save_weights(self, sess, directory):
-    #
-    #     w_data = sess.run(self.w)
-    #     np.save(directory + '/' + self.name + '-weights', w_data)
-    #     np.save(directory + '/' + self.name + '-prune-mask', self.pruning_mask_data)
+        plt.grid(True)
+        plt.hist(valid_w_data, 100, color='0.4')
+        plt.gca().set_xlim([-0.4, 0.4])
+        plt.savefig(directory + '/' + self.name + '-' + str(iteration), dpi=100)
+        plt.gcf().clear()
+
+    def save_weights(self, sess, directory):
+
+        w_data = sess.run(self.w)
+        np.save(directory + '/' + self.name + '-weights', w_data)
+        np.save(directory + '/' + self.name + '-prune-mask', self.pruning_mask_data)
 
     # quantization
     def quantize_weights(self, sess):
+        global distances
         w_data = sess.run(self.w)
         # theoretically pruning mask should be taken into consideration to compute max and min data only among valid
         # weights but in practice with normal ditribution init there is 100% chances that min and max vals will be
@@ -77,7 +86,6 @@ class LayerTrain(object):
                 distances = np.transpose(distances, (1, 2, 0))
 
             classes = np.argmin(distances, axis=-1)
-            self.clusters_masks = []
             for i in range(self.N_clusters):
                 cluster_mask = (classes == i).astype(np.float32) * self.pruning_mask_data
                 self.clusters_masks.append(cluster_mask)
@@ -161,29 +169,40 @@ class DenseLayer(object):
 
 
 class ConvLayer(object):
+
     def __init__(self, tensor, prune_mask, H_in, W_in, stride, name, dense=False):
+
         assert tensor.shape == prune_mask.shape
 
         self.stride = stride
         self.dense = dense
 
-        if not self.dense:
+        if self.dense == False:
             indices, values, dense_shape = self.tensor_to_matrix(tensor, prune_mask, H_in, W_in, stride)
+            dense_shape[1] = int(round(dense_shape[1]))
             self.w_matrix = tf.SparseTensor(indices, values, dense_shape)  # tf sparse matrix
+
         else:
             matrix = self.tensor_to_matrix(tensor, prune_mask, H_in, W_in, stride)
             self.w_matrix = tf.constant(matrix)  # tf dense matrix
 
         self.w_tensor = tf.constant(tensor * prune_mask)
 
+        print('layer:', name)
+        print('\tvalid matrix weights:', int(np.sum(prune_mask)))
+        print('\ttotal tensor weights:', np.product(self.w_tensor.shape))
+        print('\ttotal matrix weights:', np.product(self.w_matrix.shape))
+
     def get_linear_pos(self, i, j, W):  # row major
+
         return i * W + j
 
     def tensor_to_matrix(self, tensor, prune_mask, H_in, W_in, stride):
+
         # assume padding type 'SAME' and padding value 0
 
-        H_out = int(H_in + 1) / stride  # padding 'SAME'
-        W_out = int(W_in + 1) / stride  # padding 'SAME'
+        H_out = int(int(H_in + 1) / stride)  # padding 'SAME'
+        W_out = int(int(W_in + 1) / stride)  # padding 'SAME'
         H_in = int(H_in)
         W_in = int(W_in)
 
@@ -193,60 +212,70 @@ class ConvLayer(object):
         self.H_out = H_out
         self.W_out = W_out
 
-        if not self.dense:
-            indices = []
-            values = []
-            dense_shape = [], [], [H_in * W_in * D_in, H_out * W_out * D_out]  # sparse matrix
+        if self.dense == False:
+            indices, values, dense_shape = [], [], [H_in * W_in * D_in, H_out * W_out * D_out]  # sparse matrix
         else:
             matrix = np.zeros((H_in * W_in * D_in, H_out * W_out * D_out), dtype=np.float32)  # dense matrix
 
         for d_in in range(D_in):
             for d_out in range(D_out):
+
                 # tf.nn.conv2d implementation doesn't go from top-left spatial location but from bottom-right
                 for i_in_center in np.arange(H_in - 1, -1, -stride):  # kernel input center for first axis
                     for j_in_center in np.arange(W_in - 1, -1, -stride):  # kernel input center for second axis
+
                         i_out = int(i_in_center / stride)
                         j_out = int(j_in_center / stride)
+
                         for i in range(kH):
-                            i_in = i_in_center + i - kH / 2
+
+                            i_in = int(i_in_center + i - kH / 2)
+
                             if i_in < 0 or i_in >= H_in:  # padding value 0
                                 continue
 
                             for j in range(kW):
-                                j_in = j_in_center + j - kW / 2
+
+                                j_in = int(j_in_center + j - kW / 2)
+
                                 if j_in < 0 or j_in >= W_in:  # padding value 0
                                     continue
+
                                 # pruning mask: ones - valid weights, zero - pruned weights
-                                # if prune_mask[i][j][d_in][d_out] == 0.0:
-                                #     continue
+                                if prune_mask[i][j][d_in][d_out] == 0.0:
+                                    continue
 
-                                pos_in = self.get_linear_pos(i_in, j_in, W_in) + d_in * H_in * W_in
-                                pos_out = self.get_linear_pos(i_out, j_out, W_out) + d_out * H_out * W_out
+                                pos_in = int(self.get_linear_pos(i_in, j_in, W_in) + d_in * H_in * W_in)
+                                pos_out = int(self.get_linear_pos(i_out, j_out, W_out) + d_out * H_out * W_out)
 
-                                if not self.dense:
+                                if self.dense == False:
                                     indices.append([pos_in, pos_out])
                                     values.append(tensor[i][j][d_in][d_out])
                                 else:
                                     matrix[pos_in][pos_out] = tensor[i][j][d_in][d_out]
-        if not self.dense:
+
+        if self.dense == False:
             return indices, values, dense_shape
         else:
             return matrix
 
     def forward_matmul_preprocess(self, x):
+
         x = tf.transpose(x, (0, 3, 1, 2))
         x = tf.reshape(x, (-1, np.product(x.shape[1:])))
 
         return x
 
     def forward_matmul_postprocess(self, x):
-        x = tf.reshape(x, (-1, self.D_out, self.H_out, self.W_out))
+
+        x = tf.reshape(x, [-1, self.D_out, int(self.H_out), int(self.W_out)])
         x = tf.transpose(x, (0, 2, 3, 1))
 
         return x
 
     def forward_matmul(self, x):
-        if not self.dense:
+
+        if self.dense == False:
             w = tf.sparse.transpose(self.w_matrix, (1, 0))
             x = tf.transpose(x, (1, 0))
             x = tf.sparse.sparse_dense_matmul(w, x)  # only left matrix can be sparse hence transpositions
@@ -257,7 +286,112 @@ class ConvLayer(object):
         return x
 
     def forward_conv(self, x):
+
         return tf.nn.conv2d(x, self.w_tensor, strides=[1, self.stride, self.stride, 1], padding='SAME')
+
+# class ConvLayer(object):
+#     def __init__(self, tensor, prune_mask, H_in, W_in, stride, name, dense=False):
+#         assert tensor.shape == prune_mask.shape
+#
+#         self.stride = stride
+#         self.dense = dense
+#
+#         if not self.dense:
+#             indices, values, dense_shape = self.tensor_to_matrix(tensor, prune_mask, H_in, W_in, stride)
+#             # for i in range(len(indices)):
+#             #     for j in range(0, 2):
+#             #         indices[i][j] = int(np.round(indices[i][j]))
+#             # for i in range(0, 2):
+#             dense_shape[1] = int(round(dense_shape[1]))
+#             #     dense_shape[i] = int(np.round(dense_shape[i]))
+#             self.w_matrix = tf.SparseTensor(indices, values, dense_shape)  # tf sparse matrix
+#         else:
+#             matrix = self.tensor_to_matrix(tensor, prune_mask, H_in, W_in, stride)
+#             self.w_matrix = tf.constant(matrix)  # tf dense matrix
+#
+#         self.w_tensor = tf.constant(tensor * prune_mask)
+#
+#     def get_linear_pos(self, i, j, W):  # row major
+#         return i * W + j
+#
+#     def tensor_to_matrix(self, tensor, prune_mask, H_in, W_in, stride):
+#         # assume padding type 'SAME' and padding value 0
+#
+#         global indices, values, matrix, dense_shape
+#         H_out = int(H_in + 1) / stride  # padding 'SAME'
+#         W_out = int(W_in + 1) / stride  # padding 'SAME'
+#         H_in = int(H_in)
+#         W_in = int(W_in)
+#
+#         kH, kW, D_in, D_out = tensor.shape
+#
+#         self.D_out = D_out
+#         self.H_out = H_out
+#         self.W_out = W_out
+#
+#         if not self.dense:
+#             indices, values, dense_shape = [], [], [H_in * W_in * D_in, H_out* W_out * D_out] # sparse matrix
+#         else:
+#             matrix = np.zeros((H_in * W_in * D_in, H_out * W_out * D_out), dtype=np.float32)  # dense matrix
+#
+#         for d_in in range(D_in):
+#             for d_out in range(D_out):
+#                 # tf.nn.conv2d implementation doesn't go from top-left spatial location but from bottom-right
+#                 for i_in_center in np.arange(H_in - 1, -1, -stride):  # kernel input center for first axis
+#                     for j_in_center in np.arange(W_in - 1, -1, -stride):  # kernel input center for second axis
+#                         i_out = int(i_in_center / stride)
+#                         j_out = int(j_in_center / stride)
+#                         for i in range(kH):
+#                             i_in = i_in_center + i - kH / 2
+#                             if i_in < 0 or i_in >= H_in:  # padding value 0
+#                                 continue
+#
+#                             for j in range(kW):
+#                                 j_in = j_in_center + j - kW / 2
+#                                 if j_in < 0 or j_in >= W_in:  # padding value 0
+#                                     continue
+#                                 # pruning mask: ones - valid weights, zero - pruned weights
+#                                 if prune_mask[i][j][d_in][d_out] == 0.0:
+#                                     continue
+#
+#                                 pos_in = int(self.get_linear_pos(i_in, j_in, W_in) + d_in * H_in * W_in)
+#                                 pos_out = int(self.get_linear_pos(i_out, j_out, W_out) + d_out * H_out * W_out)
+#
+#                                 if not self.dense:
+#                                     indices.append([pos_in, pos_out])
+#                                     values.append(tensor[i][j][d_in][d_out])
+#                                 else:
+#                                     matrix[pos_in][pos_out] = tensor[i][j][d_in][d_out]
+#         if not self.dense:
+#             return np.round(indices), values, dense_shape
+#         else:
+#             return matrix
+#
+#     def forward_matmul_preprocess(self, x):
+#         x = tf.transpose(x, (0, 3, 1, 2))
+#         x = tf.reshape(x, (-1, np.product(x.shape[1:])))
+#
+#         return x
+#
+#     def forward_matmul_postprocess(self, x):
+#         x = tf.reshape(x, [-1, self.D_out, int(self.H_out), int(self.W_out)])
+#         x = tf.transpose(x, (0, 2, 3, 1))
+#
+#         return x
+#
+#     def forward_matmul(self, x):
+#         if not self.dense:
+#             w = tf.sparse.transpose(self.w_matrix, (1, 0))
+#             x = tf.transpose(x, (1, 0))
+#             x = tf.sparse.sparse_dense_matmul(w, x)  # only left matrix can be sparse hence transpositions
+#             x = tf.transpose(x, (1, 0))
+#         else:
+#             x = tf.matmul(x, self.w_matrix)
+#
+#         return x
+#
+#     def forward_conv(self, x):
+#         return tf.nn.conv2d(x, self.w_tensor, strides=[1, self.stride, self.stride, 1], padding='SAME')
 
 
 class NNModel:
