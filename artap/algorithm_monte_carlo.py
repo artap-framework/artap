f220a2552b0c11cc0f9b8162a7161c86aa6476c7
Hamid <neghab@fel.zcu.cz>
2021-11-19 15:28:14 +0100

    Adding simple monte carlo, modifying monte carlo for integral approximation, and tests.

--- artap/algorithm_monte_carlo.py
+++ artap/algorithm_monte_carlo.py
@@ -1,37 +1,120 @@
 import numpy as np
-import random
-
 from .algorithm import Algorithm
 from .problem import Problem
 import sys
 from .algorithm_genetic import GeneralEvolutionaryAlgorithm
 from .individual import Individual
-from artap.operators import CustomGenerator, UniformGenerator, RandomGenerator
+from artap.operators import RandomGenerator
+from artap.benchmark_functions import BenchmarkFunction as bm
 import time
 
 
 class Monte_Carlo(Algorithm):
+    """
+    Monte Carlo simulation
+        1) make a list of random values of x
+        2) pick a small number of random value of x
+        3) evaluate the objective function
+        4) pick the lowest
+    """
     def __init__(self, problem: Problem, n):
         super().__init__(problem)
         self.evaluation = 0
         self.z_min, self.z_max = 0, 1
+        self.dimension = 1
         self.n = n  # number of divisions
         self.sampling_size = 10  # 1000 sampling points in each division
         self.problem = problem
+        self.problem.parameters = bm.generate_paramlist(self, dimension=self.dimension, lb=0.0, ub=1.0)
         self.intervals = np.linspace(self.z_min, self.z_max, self.n)
         self.generator = RandomGenerator(self.problem.parameters)
 
-    def loop(self):
+    def generate(self):
+        costs = []
+        best_vector = []
+        for _ in range(self.sampling_size):
+            self.generator.init(self.n)
+            self.individuals = self.generator.generate()
+            self.evaluate(self.individuals)
+            for individual in self.individuals:
+                costs.append(individual.costs)
+                best_vector.append(individual.vector)
+                # for individual in self.individuals:
+                # append to problem
+                self.problem.individuals.append(individual)
+                # add to population
+                individual.population_id = 0
+
+                self.problem.data_store.sync_individual(individual)
+        costs = np.array(costs).reshape(-1, 1)
+        best_vector = np.array(best_vector).reshape(-1, 1)
+        return costs, best_vector
+
+    def simulate(self):
+        var_array = []
+        I_array = []
+        for i in range(self.n - 1):
+            # random sampling in each of the interval
+            costs, vectors = self.generate()
+            I_array.append(min(costs))  # add up the integral value
+            index = np.argmin(costs)
+            var_array.append(vectors[index])
+
+        return I_array, var_array
+
+    def run(self):
+        I_array, var_array = self.simulate()
+        self.evaluation += (len(self.intervals) - 1) * self.sampling_size
+        new_I = min(I_array)
+        index = np.argmin(I_array)
+        new_var = var_array[index]
+        relative_accuracy = 1  # assign a non-zero value of initial relative accuracy
+        while relative_accuracy >= 1e-2:
+            old_I = new_I
+            I_array, var_array = self.simulate()
+            new_I = min(I_array)
+            index = np.argmin(I_array)
+            new_var = var_array[index]
+            # calculate relative accuracy
+            relative_accuracy = abs((new_I - old_I) / old_I)
+
+        for i in range(self.n - 1):
+            for individual in self.individuals:
+                # add to population
+                individual.population_id = i + 1
+                # append to problem
+                self.problem.individuals.append(individual)
+                # sync to datastore
+                self.problem.data_store.sync_individual(individual)
+
+        return new_I, new_var
+
+
+class Integral_Monte_Carlo(Algorithm):
+    """
+    Adaptive Monte Carlo Integral Approximation.
+    Summation is used to estimate the expected value of a function under a distribution, instead of integration.
+    """
+    def __init__(self, problem: Problem, n):
+        super().__init__(problem)
+        self.evaluation = 0
+        self.z_min, self.z_max = 0, 1
+        self.dimension = 1
+        self.n = n  # number of divisions
+        self.sampling_size = 10  # 1000 sampling points in each division
+        self.problem = problem
+        self.problem.parameters = bm.generate_paramlist(self, dimension=self.dimension, lb=0.0, ub=1.0)
+        self.intervals = np.linspace(self.z_min, self.z_max, self.n)
+        self.generator = RandomGenerator(self.problem.parameters)
+
+    def generate(self):
         elements = []
         for _ in range(self.sampling_size):
-            # z = random.uniform(lower, upper)
-            # self.generator.init(int(z*100))
             self.generator.init(self.n)
             self.individuals = self.generator.generate()
             self.evaluate(self.individuals)
             for individual in self.individuals:
                 elements.append(individual.costs)
-            for individual in self.individuals:
                 # append to problem
                 self.problem.individuals.append(individual)
                 # add to population
@@ -41,12 +124,12 @@ class Monte_Carlo(Algorithm):
         elements = np.array(elements).reshape(-1, 1)
         return elements
 
-    def monte_carlo(self):
+    def simulate(self):
         var_array = []
         I_array = []
-        for i in range(len(self.intervals) - 1):
+        for i in range(self.n - 1):
             # random sampling in each of the interval
-            elements = self.loop()
+            elements = self.generate()
             # integral of segment of integration
             average = sum(elements) / self.sampling_size
             # weight of integral is correspond to the width of the sub-interval
@@ -60,7 +143,7 @@ class Monte_Carlo(Algorithm):
         return I_array, var_array
 
     def run(self):
-        I_array, var_array = self.monte_carlo()
+        I_array, var_array = self.simulate()
         self.evaluation += (len(self.intervals) - 1) * self.sampling_size
         new_I = sum(I_array)
         relative_accuracy = 1  # assign a non-zero value of initial relative accuracy
@@ -82,7 +165,7 @@ class Monte_Carlo(Algorithm):
                                                    endpoint=False))
             self.intervals = np.delete(self.intervals, largest_var_index)
             # run Monte Carlo in the new intervals
-            I_array, var_array = self.monte_carlo()
+            I_array, var_array = self.simulate()
             new_I = sum(I_array)
             # calculate relative accuracy
             relative_accuracy = abs((new_I - old_I) / old_I)
@@ -92,7 +175,7 @@ class Monte_Carlo(Algorithm):
             # print((len(intervals)-1)*sampling_size,new_I,relative_accuracy)    #
             # show realtime evaluations
         err = 0
-        for i in range(len(self.intervals) - 1):
+        for i in range(self.n - 1):
             # sum up the variance of each interval
             err += ((self.intervals[i + 1] - self.intervals[i]) /
                     (self.z_max - self.z_min)) ** 2 * var_array[i]
@@ -108,4 +191,4 @@ class Monte_Carlo(Algorithm):
         # of estimation)
 
         err = np.sqrt(err / (len(self.intervals) * self.sampling_size))
-        return new_I, err, self.evaluation
+        return self.evaluation, new_I
--- artap/tests/test_problem_monte_carlo.py
+++ artap/tests/test_problem_monte_carlo.py
@@ -1,7 +1,26 @@
 import unittest
-from artap.algorithm_monte_carlo import Monte_Carlo
+from artap.algorithm_monte_carlo import Monte_Carlo, Integral_Monte_Carlo
 from artap.results import Results
-from artap.benchmark_functions import Sphere, Ackley
+from artap.benchmark_functions import Sphere, Ackley, BenchmarkFunction
+import numpy as np
+
+
+class Integral_Problem(BenchmarkFunction):
+    def set(self, **kwargs):
+        """Time-dependent 1D QM wave function of a single particle - squared."""
+        self.set_dimension(**kwargs)
+        self.parameters = self.generate_paramlist(self.dimension, lb=0.0, ub=1.0)
+
+        self.global_optimum = 0.0
+        self.global_optimum_coords = [0.0 for x in range(self.dimension)]
+
+        # single objective problem
+        self.costs = [{'name': 'f_1', 'criteria': 'minimize'}]
+
+    def evaluate(self, z):
+        x = z.vector
+        output = np.exp(-x[0] ** 2) / (np.pi ** 0.5)
+        return [output]
 
 
 class TestAckley(unittest.TestCase):
@@ -38,6 +57,21 @@ class TestSphere(unittest.TestCase):
         print(optimum.costs[0])
         print(problem.global_optimum)
         self.assertAlmostEqual(optimum.costs[0], problem.global_optimum)
+
+
+class TestIntegral_MonteCarlo(unittest.TestCase):
+    # unit-test  benchmarck : Sphere, algorithm : Monte_Carlo
+    def test_local_problem(self):
+        problem = Integral_Problem(**{'dimension': 1})
+        n = 10
+        algorithm = Integral_Monte_Carlo(problem, n)
+        algorithm.run()
+
+        result = Results(problem)
+        optimum = result.find_optimum('f_1')
+        print(optimum.costs[0])
+        print(problem.global_optimum)
+        self.assertAlmostEqual(int(optimum.costs[0]), problem.global_optimum)
 
 
 if __name__ == '__main__':
